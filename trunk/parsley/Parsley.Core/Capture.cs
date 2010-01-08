401c06402dbc3f4968a444d0f9299fd9b83c62c9
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-08 11:49:16 +0000

    initial commit

---
+++ trunk/parsley/Parsley.Core/Capture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Parsley.Core {
+
+  /// <summary>
+  /// Capture from camera
+  /// </summary>
+  public class Capture : Resource {
+
+    private Emgu.CV.Capture _capture;
+
+    /// <summary>
+    /// Capture from camera with specific index starting at 0.
+    /// </summary>
+    public static Capture FromCamera(int index) {
+      return new Capture(new Emgu.CV.Capture(index));
+    }
+
+    /// <summary>
+    /// Capture from movie saved on disk
+    /// </summary>
+    public static Capture FromMovie(string filename) {
+      return new Capture(new Emgu.CV.Capture(filename));
+    }
+
+    /// <summary>
+    /// Capture from resource
+    /// </summary>
+    Capture(Emgu.CV.Capture detail) {
+      _capture = detail;
+    }
+
+    /// <summary>
+    /// Return the next colored frame.
+    /// </summary>
+    /// <returns></returns>
+    public Image<Bgr, Byte> Frame() {
+      return _capture.QueryFrame();
+    }
+
+
+    protected override void DisposeManaged() {
+      _capture.Dispose();
+    }
+  }
+}
---
+++ trunk/parsley/Parsley.Core/Resource.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parsley.Core {
+
+  /// <summary>
+  /// A resource is a disposable object. 
+  /// </summary>
+  public class Resource : IDisposable {
+    private bool _disposed;
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public Resource() {
+      _disposed = false;
+    }
+    
+    /// <summary>
+    /// C# finalization code.
+    /// Do not override in child classes.
+    /// </summary>
+    ~Resource() {
+      // This destructor will run only if the Dispose method
+      // does not get called.
+      this.Dispose(false);
+    }
+
+
+    /// <summary>
+    /// Dispose resource explicitly.
+    /// </summary>
+    public void Dispose() {
+      // Release managed and unmanaged resources
+      Dispose(true);
+      // Take object off the finalization queue
+      GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Disposing called from Dispose or the garbage collector.
+    /// </summary>
+    /// <remarks>
+    /// Dispose is either called by the user invoking Dispose, or by
+    /// the garbage collector. If called by the user, disposing will be true.
+    /// If called by the garbage collector, disposing will be set to false.
+    /// </remarks>
+    private void Dispose(bool disposing) {
+      // If not yet disposed
+      if (!_disposed) {
+        // If disposing equals true, dispose all managed
+        // and unmanaged resources.
+        if (disposing) {
+          DisposeManaged();
+        }
+        // Call the appropriate methods to clean up
+        // unmanaged resources here.
+        DisposeUnmanaged();
+
+        _disposed = true;
+      }
+    }
+
+    /// <summary>
+    /// Release, dispose, other managed resources acquired by this object
+    /// </summary>
+    protected virtual void DisposeManaged() {
+    }
+
+    /// <summary>
+    /// Release, dispose, unmanaged resources acquired by this object
+    /// </summary>
+    protected virtual void DisposeUnmanaged() {
+    }
+  }
+}
