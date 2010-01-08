c5fbeddd017b60c35737c2face990c14faabf90e
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-08 15:46:49 +0000

    added more examples

---
+++ trunk/Parsley.Core/CheckerBoard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV.Structure;
+
+namespace Parsley.Core {
+
+  /// <summary>
+  /// Represents a checkerboard used for calibration
+  /// </summary>
+  public class CheckerBoard {
+    private System.Drawing.Size _inner_corners;
+    private System.Drawing.PointF[] _corners;
+    private bool _pattern_found;
+
+    /// <summary>
+    /// Construct checkboard from the number of inner corners per row and column.
+    /// </summary>
+    public CheckerBoard(int corner_row, int corner_column) {
+      _inner_corners = new System.Drawing.Size(corner_row, corner_column);
+      _pattern_found = false;
+    }
+
+    /// <summary>
+    /// Locate the checkerboard pattern in the provided image.
+    /// </summary>
+    /// <param name="img"></param>
+    /// <returns>True if pattern was found</returns>
+    public void FindPattern(Emgu.CV.Image<Gray, Byte> img) {
+      _pattern_found = Emgu.CV.CameraCalibration.FindChessboardCorners(
+        img, 
+        _inner_corners, 
+        Emgu.CV.CvEnum.CALIB_CB_TYPE.ADAPTIVE_THRESH,
+        out _corners);
+    }
+
+    public void Draw(Emgu.CV.Image<Bgr, Byte> img, int size, int thickness) {
+      System.Drawing.Color color = _pattern_found ? System.Drawing.Color.Green : System.Drawing.Color.Red; 
+      Bgr bgr = new Bgr(color);
+      int count = 1;
+      foreach (System.Drawing.PointF corner in _corners) {
+        img.Draw(new CircleF(corner, size), bgr, thickness);
+        count++;
+      }
+    }
+  }
+}
---
+++ trunk/example/FindCheckerboard/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FindCheckerboard {
+  public partial class Form1 : Form {
+
+    private Parsley.Core.Capture _capture;
+    private Parsley.Core.CheckerBoard _cb;
+
+    public Form1() {
+      InitializeComponent();
+      _capture = Parsley.Core.Capture.FromCamera(0);
+      _cb = new Parsley.Core.CheckerBoard(9, 6);
+      Application.Idle += new EventHandler(Application_Idle);
+    }
+
+    void Application_Idle(object sender, EventArgs e) {
+      Image<Bgr, Byte> img = _capture.Frame();
+      Image<Gray, Byte> gray = img.Convert<Gray, Byte>();
+      gray._EqualizeHist();
+
+      _cb.FindPattern(gray);
+      _cb.Draw(img, 4, 2);
+
+      _image_box.Image = img;
+      //.Image = _capture.Frame();
+    }
+  }
+}
