e0e257d5c4be42878822f54ebd05a9649869a623
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-09 12:14:44 +0000

    removed ununsed stdafx

--- Parsley.Draw3D/Stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
-// Parsley.Draw3D.pch ist der vorkompilierte Header.
-// stdafx.obj enthält die vorkompilierten Typinformationen.
-
-#include "stdafx.h"
