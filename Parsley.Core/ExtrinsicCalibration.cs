87492f7af103aedb99572f7dce17b24c9888ea7b
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-09 17:22:35 +0000

    added missing files and ignores

---
+++ Parsley.Core/ExtrinsicCalibration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV.Structure;
+
+namespace Parsley.Core {
+
+  /// <summary>
+  /// Perform extrinsic calibration
+  /// </summary>
+  public class ExtrinsicCalibration : CalibrationBase {
+    private Emgu.CV.IntrinsicCameraParameters _intrinsics;
+
+    public ExtrinsicCalibration(MCvPoint3D32f[] object_points, Emgu.CV.IntrinsicCameraParameters intrinsics)
+      : base(object_points) {
+      _intrinsics = intrinsics;
+    }
+
+    public Emgu.CV.ExtrinsicCameraParameters Calibrate(System.Drawing.PointF[] image_points) {
+      return Emgu.CV.CameraCalibration.FindExtrinsicCameraParams2(
+       this.ObjectPoints,
+       image_points,
+       _intrinsics);
+    }
+  }
+}
