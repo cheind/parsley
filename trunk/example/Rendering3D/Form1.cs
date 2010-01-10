ba69c23b92fd4701b2908bd4fc87473b215548d2
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-10 10:11:41 +0000

    made references to dlls : no-copy and no-revision specific

--- trunk/example/Rendering3D/Form1.cs
+++ trunk/example/Rendering3D/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Emgu.CV.Structure;
+
 namespace Rendering3D {
   public partial class Form1 : Form {
     private Parsley.Draw3D.Viewer _viewer;
@@ -16,10 +18,21 @@ namespace Rendering3D {
     public Form1() {
       InitializeComponent();
       _viewer = new Parsley.Draw3D.Viewer(_render_target);
+      _viewer.LookAt(new MCvPoint3D32f(50.0f, 50.0f, 50.0f), new MCvPoint3D32f(0, 0, 0), new MCvPoint3D32f(0.0f, 1.0f, 0.0f));
       _rnd = new Random();
       _bw = new BackgroundWorker();
+      _bw.WorkerSupportsCancellation = true;
       _bw.DoWork += new DoWorkEventHandler(Render);
+      _bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_bw_RunWorkerCompleted);
       _bw.RunWorkerAsync();
+    }
+
+    void _bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+      if (e.Cancelled) {
+        // Cancelled through form closing
+        _viewer.Dispose();
+        this.Close();
+      }
     }
 
     void Render(object sender, DoWorkEventArgs e) {
@@ -32,9 +45,17 @@ namespace Rendering3D {
       e.Cancel = true;
     }
 
+    protected override void OnFormClosing(FormClosingEventArgs e) {
+      if (_bw.IsBusy) {
+        _bw.CancelAsync();
+        e.Cancel = true;
+        this.Enabled = false;
+      }
+    }
+
     private void _button_add_capsule_Click(object sender, EventArgs e) {
       System.Threading.Monitor.Enter(_viewer);
-      _viewer.AddCapsule(_rnd.Next(-10, 10), _rnd.Next(-10, 10), _rnd.Next(-10, 10));
+      _viewer.AddCapsule(new MCvPoint3D32f(_rnd.Next(-10, 10), _rnd.Next(-10, 10), _rnd.Next(-10, 10)));
       System.Threading.Monitor.Exit(_viewer);
     }
   }
