0649dba9e9ad25e1a91dcb455466a5b568899f20
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-11 20:15:36 +0000

    initial commit of new controls

---
+++ trunk/Parsley.UI/Concrete/FrameGrabber.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Parsley.UI.Concrete {
+  partial class FrameGrabber {
+    /// <summary> 
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Komponenten-Designer generierter Code
+
+    /// <summary> 
+    /// Erforderliche Methode für die Designerunterstützung. 
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this._picture_box = new Emgu.CV.UI.ImageBox();
+      ((System.ComponentModel.ISupportInitialize)(this._picture_box)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // _picture_box
+      // 
+      this._picture_box.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._picture_box.Location = new System.Drawing.Point(0, 0);
+      this._picture_box.Name = "_picture_box";
+      this._picture_box.Size = new System.Drawing.Size(544, 381);
+      this._picture_box.TabIndex = 2;
+      this._picture_box.TabStop = false;
+      // 
+      // FrameGrabber
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this._picture_box);
+      this.Name = "FrameGrabber";
+      this.Size = new System.Drawing.Size(544, 381);
+      ((System.ComponentModel.ISupportInitialize)(this._picture_box)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private Emgu.CV.UI.ImageBox _picture_box;
+  }
+}
---
+++ trunk/Parsley.UI/Concrete/FrameGrabber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parsley.UI.Concrete {
+  public partial class FrameGrabber : UserControl {
+    private Core.FrameGrabber _grabber;
+
+    public FrameGrabber() {
+      InitializeComponent();
+    }
+
+    public void GrabFrom(Core.FrameGrabber fg) {
+      _grabber = fg;
+      _grabber.OnFrame += new Parsley.Core.FrameGrabber.OnFrameHandler(_grabber_OnFrame);
+    }
+
+    void _grabber_OnFrame(Parsley.Core.FrameGrabber fg, Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> img) {
+      _picture_box.Image = img;
+    }
+  }
+}
