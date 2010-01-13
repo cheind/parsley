2955c3bbb35a8abc4af51df416b590a2fb153765
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-13 21:36:56 +0000

    initial commit of laser line extraction algorithms

---
+++ trunk/Parsley.Core/BrightestPixelLLE.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV.Structure;
+
+namespace Parsley.Core {
+  /// <summary>
+  /// Laser line extraction based on the brightest pixel
+  /// </summary>
+  /// <remarks>
+  /// Algorithms scans reports the brightest, maximum intensity, y-value per x-value 
+  /// if it exceeds a user controlable threshold. If no y-value exceeds the threshold,
+  /// the algorithm reports -1.0f for this x-value. This algorithm is implemented using 
+  /// a single pass.
+  /// 
+  /// The image memory layout should have a column-major layout in order to reduce
+  /// chaching misses. OpenCV stores images as one-dimensional array, in row-major
+  /// form. Todo: Consider flipping the image and providing a Flipped property. When
+  /// using the flipped property the resulting laser points will be adapted to
+  /// conform with the original image.
+  /// </remarks>
+  public class BrightestPixelLLE : LaserLineExtraction {
+    private int _threshold;
+
+    /// <summary>
+    /// Initialize with no threshold
+    /// </summary>
+    public BrightestPixelLLE() {
+      _threshold = 0;
+    }
+
+    /// <summary>
+    /// Initialize with threshold
+    /// </summary>
+    /// <param name="threshold">Minimum intensity threshold</param>
+    public BrightestPixelLLE(int threshold) {
+      _threshold = threshold;
+    }
+
+    /// <summary>
+    /// Get and set the intensity threshold.
+    /// </summary>
+    public int IntensityThreshold {
+      get { return _threshold; }
+      set { _threshold = value; }
+    }
+
+    public override void FindLaserLine(Emgu.CV.Image<Gray, byte> channel, out System.Drawing.PointF[] laser_points) {
+      laser_points = new System.Drawing.PointF[channel.Width];
+      // Search rows
+      for (int c = 0; c < channel.Width; ++c) {
+        int max = this.FindMax(channel, c);
+        laser_points[c].X = c;
+        laser_points[c].Y = (max >= _threshold) ? max : -1;
+      }
+    }
+
+    int FindMax(Emgu.CV.Image<Gray, byte> channel, int column) {
+      int max = 0;
+      for (int k = 0; k < channel.Height; ++k) {
+        int i = (int)channel[k, column].Intensity;
+        if (i > max) max = k;
+      }
+      return max;
+    }
+
+  }
+}
