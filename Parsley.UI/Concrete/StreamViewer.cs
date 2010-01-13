35f15e9e8d87bfbbf5660376e31f3b044cb93bcc
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-13 09:06:33 +0000

    Added a Form that displays images from a FrameGrabber. StreamViewer is able to maintain the aspect ration of frames delivered by the camera during window resizing.

---
+++ Parsley.UI/Concrete/StreamViewer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parsley.UI.Concrete {
+  public partial class StreamViewer : Form {
+
+    public StreamViewer() {
+      InitializeComponent();
+      this.MaintainAspectRatio();
+    }
+
+    public Parsley.Core.FrameGrabber FrameGrabber {
+      get { return _display.FrameGrabber; }
+      set { _display.FrameGrabber = value; }
+    }
+
+    public Emgu.CV.CvEnum.INTER Interpolation {
+      get { return _display.Interpolation; }
+      set { _display.Interpolation = value; }
+    }
+
+    public void Pause() {
+      _display.Pause();
+    }
+
+    public void Resume() {
+      _display.Resume();
+    }
+
+    protected override void OnResizeBegin(EventArgs e) {
+      _display.Pause();
+      base.OnResizeBegin(e);
+    }
+
+    protected override void OnResizeEnd(EventArgs e) {
+      this.MaintainAspectRatio();
+      _display.Resume();
+      base.OnResizeEnd(e);
+    }
+
+    private void MaintainAspectRatio() {
+      if (this.FrameGrabber != null) {
+        Parsley.Core.Camera cam = this.FrameGrabber.Camera;
+        if (cam != null && !cam.Disposed) {
+          double ar = cam.FrameAspectRatio;
+          this.Height = (int)Math.Ceiling(this.Width / ar);
+        }
+      }
+    }
+  }
+}
