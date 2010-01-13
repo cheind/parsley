f470f6ddfaf00eab72076ef8d84367f3aefe6756
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-13 19:55:59 +0000

    extrinsics is now a collection. added documentation

--- Parsley.Core/Camera.cs
+++ Parsley.Core/Camera.cs
@@ -10,13 +10,17 @@ using Emgu.CV.Structure;
 namespace Parsley.Core {
   
   /// <summary>
-  /// High level access to a camera
+  /// Represents a camera.
   /// </summary>
   public class Camera : SharedResource {
     private Emgu.CV.Capture _device;
     private Emgu.CV.IntrinsicCameraParameters _intrinsics;
-    private Emgu.CV.ExtrinsicCameraParameters _extrinsics;
+    private List<Emgu.CV.ExtrinsicCameraParameters> _extrinsics;
 
+    /// <summary>
+    /// Initialize camera from device index
+    /// </summary>
+    /// <param name="device_index">Device index starting at zero.</param>
     public Camera(int device_index) {
       try {
         _device = new Emgu.CV.Capture(device_index);
@@ -24,50 +28,79 @@ namespace Parsley.Core {
         throw new ArgumentException(String.Format("No camera device found at slot {0}.", device_index));
       }
       _intrinsics = null;
-      _extrinsics = null;
+      _extrinsics = new List<ExtrinsicCameraParameters>();
     }
 
+    /// <summary>
+    /// Initialize with pre-existing device
+    /// </summary>
+    /// <remarks>Device is disposed when camera is disposed.</remarks>
+    /// <param name="device">Device to initialize from</param>
     public Camera(Emgu.CV.Capture device) {
       _device = device;
     }
 
+    /// <summary>
+    /// True if camera has an intrinsic calibration associated.
+    /// </summary>
     [Description("Determines if camera has an instrinsic calibration")]
     public bool HasIntrinsics {
       get { return _intrinsics != null; }
     }
 
+    /// <summary>
+    /// True if camera has at least one extrinsic calibration associated.
+    /// </summary>
     [Description("Determines if camera has an extrinsic calibration")]
     public bool HasExtrinsics {
-      get { return _extrinsics != null; }
+      get { return _extrinsics.Count > 0; }
     }
 
+    /// <summary>
+    /// Access intrinsic calibration
+    /// </summary>
     [Browsable(false)]
     public Emgu.CV.IntrinsicCameraParameters Intrinsics {
       get { return _intrinsics; }
       set { _intrinsics = value; }
     }
 
+    /// <summary>
+    /// Access associated extrinsic calibrations
+    /// </summary>
     [Browsable(false)]
-    public Emgu.CV.ExtrinsicCameraParameters Extrinsics {
+    public List<Emgu.CV.ExtrinsicCameraParameters> Extrinsics {
       get { return _extrinsics; }
-      set { _extrinsics = value; }
     }
 
+    /// <summary>
+    /// Frame width of device
+    /// </summary>
     [Description("Frame Width")]
     public int FrameWidth {
       get { return (int)_device.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH); }
     }
 
+    /// <summary>
+    /// Frame height of device
+    /// </summary>
     [Description("Frame Height")]
     public int FrameHeight {
       get { return (int)_device.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT); }
     }
 
+    /// <summary>
+    /// Aspect ratio of FrameWidth / FrameHeight
+    /// </summary>
     [Description("Frame Aspect Ratio of Width/Height")]
     public double FrameAspectRatio {
       get { return ((double)FrameWidth) / FrameHeight; }
     }
 
+    /// <summary>
+    /// Retrieve the current frame.
+    /// </summary>
+    /// <returns></returns>
     public Image<Bgr, Byte> Frame() {
       return _device.QueryFrame();
     }
