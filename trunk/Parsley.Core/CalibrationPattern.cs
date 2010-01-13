1fb89197f0313afc4d969742986f0234bb6747c8
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-13 15:15:51 +0000

    introduced CalibrationPattern as base class for point based calibration. Made CheckerBoard a CalibrationPattern.

---
+++ trunk/Parsley.Core/CalibrationPattern.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+using Emgu.CV.Structure;
+
+namespace Parsley.Core {
+
+  /// <summary>
+  /// A Calibration pattern
+  /// </summary>
+  public abstract class CalibrationPattern {
+    private MCvPoint3D32f[] _object_points;
+    private PointF[] _image_points;
+    private bool _pattern_found;
+
+    public CalibrationPattern() {}
+
+    /// <summary>
+    /// Get or set the object points
+    /// </summary>
+    public MCvPoint3D32f[] ObjectPoints {
+      get { return _object_points; }
+      set { _object_points = value; }
+    }
+
+    /// <summary>
+    /// Get the corresponding image points from the last call to FindPattern
+    /// </summary>
+    public PointF[] ImagePoints {
+      get { return _image_points; }
+    }
+
+    /// <summary>
+    /// True if pattern was found by last FindPattern call; false otherwise.
+    /// </summary>
+    public bool PatternFound {
+      get { return _pattern_found; }
+    }
+
+    /// <summary>
+    /// Find pattern in image and make it accessible through local properties.
+    /// </summary>
+    /// <param name="img">Image to find pattern in.</param>
+    /// <returns>True if pattern was found in image, false otherwise</returns>
+    public bool FindPattern(Emgu.CV.IImage img) {
+      _pattern_found =  this.FindPattern(img, out _image_points);
+      return _pattern_found;
+    }
+
+    /// <summary>
+    /// Find pattern in image.
+    /// </summary>
+    /// <param name="img">Image to find pattern in.</param>
+    /// <param name="image_points">Pattern points in image.</param>
+    /// <returns>True if pattern was found in image, false otherwise.</returns>
+    abstract public bool FindPattern(Emgu.CV.IImage img, out PointF[] image_points);
+
+    /// <summary>
+    /// Draw pattern to image.
+    /// </summary>
+    /// <param name="img">Colored image to draw to.</param>
+    /// <param name="image_points">Image center points.</param>
+    /// <param name="pattern_found">If true green indicators are drawn, red ones otherwise.</param>
+    public virtual void DrawPattern(Emgu.CV.Image<Bgr, Byte> img, PointF[] image_points, bool pattern_found) {
+      System.Drawing.Color color = pattern_found ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+      Bgr bgr = new Bgr(color);
+      MCvFont f = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_PLAIN, 0.8, 0.8);
+      int count = 1;
+      foreach (PointF point in image_points) {
+        img.Draw(new CircleF(point, 4), bgr, 2);
+        img.Draw(count.ToString(), ref f, new System.Drawing.Point((int)point.X + 5, (int)point.Y - 5), bgr);
+        count++;
+      }
+    }
+  };
+}
