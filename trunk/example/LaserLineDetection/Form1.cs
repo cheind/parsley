83e21e8894141b457bdd8900b551ffe12187c048
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-14 15:47:41 +0000

    adapted

--- trunk/example/LaserLineDetection/Form1.cs
+++ trunk/example/LaserLineDetection/Form1.cs
@@ -12,7 +12,7 @@ namespace LaserLineDetection {
     private Parsley.Core.Camera _camera;
     private Parsley.Core.FrameGrabber _grabber;
     private Emgu.CV.Image<Emgu.CV.Structure.Gray, Byte> _reference;
-    private Parsley.Core.LaserLineExtraction _lle;
+    private Parsley.Core.BrightestPixelLLE _lle;
     DateTime _start;
 
     public Form1() {
@@ -20,30 +20,34 @@ namespace LaserLineDetection {
       _grabber = new Parsley.Core.FrameGrabber(_camera);
       _grabber.OnFrame += new Parsley.Core.FrameGrabber.OnFrameHandler(_grabber_OnFrame);
       _lle = new Parsley.Core.BrightestPixelLLE();
+      _lle.IntensityThreshold = 20;
       this.FrameGrabber = _grabber;
       _start = DateTime.Now;
       _grabber.Start();
     }
 
     protected override void OnFormClosing(FormClosingEventArgs e) {
-      base.OnFormClosing(e);
       _grabber.RequestStop();
       _camera.Dispose();
+      base.OnFormClosing(e);
     }
 
     Emgu.CV.IImage _grabber_OnFrame(Parsley.Core.FrameGrabber fg, Emgu.CV.IImage img) {
       Emgu.CV.Image<Emgu.CV.Structure.Bgr, Byte> casted = img as Emgu.CV.Image<Emgu.CV.Structure.Bgr, Byte>;
-      Emgu.CV.Image<Emgu.CV.Structure.Gray, Byte>[] channel = casted.Split();
+      //Emgu.CV.Image<Emgu.CV.Structure.Gray, Byte>[] channel = casted.Split();
+      Emgu.CV.Image<Emgu.CV.Structure.Gray, Byte> gray = casted.Convert<Emgu.CV.Structure.Gray, Byte>();
       if (_reference == null) {
         if ((DateTime.Now - _start).Seconds > 5) {
-          _reference = channel[2];
+          //_reference = channel[1];
+          _reference = gray;
         }
       } else {
         Emgu.CV.Image<Emgu.CV.Structure.Gray, Byte> result = new Emgu.CV.Image<Emgu.CV.Structure.Gray, byte>(img.Size);
-        _lle.FindLaserLine(channel[2].Sub(_reference));
+        //_lle.FindLaserLine(channel[1].Sub(_reference));
+        _lle.FindLaserLine(gray);
         foreach (PointF p in _lle.LaserPoints) {
           if (p.Y >= 0.0f) {
-            result[(int)p.Y,(int)p.X] = new Emgu.CV.Structure.Gray(200);
+            result[(int)p.Y,(int)p.X] = new Emgu.CV.Structure.Gray(100);
           }
         }
         img = result;
