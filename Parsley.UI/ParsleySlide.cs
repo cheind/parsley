92c4098fa2cb077897ef6efd096d21da97896434
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-15 22:10:12 +0000

    added parsley slide (adjusts font)

---
+++ Parsley.UI/ParsleySlide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parsley.UI {
+  public partial class ParsleySlide : Slide {
+    public ParsleySlide() {
+      InitializeComponent();
+    }
+  }
+}
