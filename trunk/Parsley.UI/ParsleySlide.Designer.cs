a0fd33ae1353807fc87349354d2ec0849d0591d6
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-15 22:10:12 +0000

    added parsley slide (adjusts font)

---
+++ trunk/Parsley.UI/ParsleySlide.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Parsley.UI {
+  partial class ParsleySlide {
+    /// <summary> 
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Komponenten-Designer generierter Code
+
+    /// <summary> 
+    /// Erforderliche Methode für die Designerunterstützung. 
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent() {
+      this.SuspendLayout();
+      // 
+      // ParsleySlide
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.Name = "ParsleySlide";
+      this.Size = new System.Drawing.Size(200, 219);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
