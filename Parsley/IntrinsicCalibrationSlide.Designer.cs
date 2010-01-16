33cd5b7a7743725f08d96811799fb5304e3ba087
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-16 09:19:28 +0000

    Extended intrinsic calibration slide

--- Parsley/IntrinsicCalibrationSlide.Designer.cs
+++ Parsley/IntrinsicCalibrationSlide.Designer.cs
@@ -25,63 +25,49 @@
     private void InitializeComponent() {
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IntrinsicCalibrationSlide));
       this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-      this._lbl_info = new Parsley.UI.GrowLabel();
-      this.parsleyButton1 = new Parsley.UI.ParsleyButton();
+      this._cb_auto_take = new System.Windows.Forms.CheckBox();
       this._btn_take_image = new Parsley.UI.ParsleyButton();
-      this.parsleyButton2 = new Parsley.UI.ParsleyButton();
+      this._btn_calibrate = new Parsley.UI.ParsleyButton();
+      this._lbl_info = new Parsley.UI.GrowLabel();
       this.tableLayoutPanel1.SuspendLayout();
       this.SuspendLayout();
       // 
       // tableLayoutPanel1
       // 
       this.tableLayoutPanel1.ColumnCount = 1;
-      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-      this.tableLayoutPanel1.Controls.Add(this._lbl_info, 0, 2);
-      this.tableLayoutPanel1.Controls.Add(this.parsleyButton1, 0, 1);
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 441F));
+      this.tableLayoutPanel1.Controls.Add(this._cb_auto_take, 0, 1);
       this.tableLayoutPanel1.Controls.Add(this._btn_take_image, 0, 0);
-      this.tableLayoutPanel1.Controls.Add(this.parsleyButton2, 0, 3);
+      this.tableLayoutPanel1.Controls.Add(this._btn_calibrate, 0, 3);
+      this.tableLayoutPanel1.Controls.Add(this._lbl_info, 0, 2);
       this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
       this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
       this.tableLayoutPanel1.Name = "tableLayoutPanel1";
       this.tableLayoutPanel1.RowCount = 4;
-      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-      this.tableLayoutPanel1.Size = new System.Drawing.Size(391, 341);
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(441, 370);
       this.tableLayoutPanel1.TabIndex = 0;
       // 
-      // _lbl_info
+      // _cb_auto_take
       // 
-      this._lbl_info.Anchor = System.Windows.Forms.AnchorStyles.None;
-      this._lbl_info.Location = new System.Drawing.Point(45, 203);
-      this._lbl_info.Name = "_lbl_info";
-      this._lbl_info.Size = new System.Drawing.Size(300, 19);
-      this._lbl_info.TabIndex = 4;
-      this._lbl_info.Text = "Start calibration by taking images";
-      // 
-      // parsleyButton1
-      // 
-      this.parsleyButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
-      this.parsleyButton1.BackColor = System.Drawing.Color.White;
-      this.parsleyButton1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
-      this.parsleyButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-      this.parsleyButton1.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.parsleyButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-      this.parsleyButton1.Image = ((System.Drawing.Image)(resources.GetObject("parsleyButton1.Image")));
-      this.parsleyButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-      this.parsleyButton1.Location = new System.Drawing.Point(45, 97);
-      this.parsleyButton1.Name = "parsleyButton1";
-      this.parsleyButton1.Size = new System.Drawing.Size(300, 60);
-      this.parsleyButton1.TabIndex = 1;
-      this.parsleyButton1.Text = "Take Auto Images";
-      this.parsleyButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-      this.parsleyButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-      this.parsleyButton1.UseVisualStyleBackColor = true;
+      this._cb_auto_take.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this._cb_auto_take.AutoSize = true;
+      this._cb_auto_take.Location = new System.Drawing.Point(147, 116);
+      this._cb_auto_take.Name = "_cb_auto_take";
+      this._cb_auto_take.Size = new System.Drawing.Size(147, 23);
+      this._cb_auto_take.TabIndex = 9;
+      this._cb_auto_take.Text = "Auto take images";
+      this._cb_auto_take.UseVisualStyleBackColor = true;
+      this._cb_auto_take.CheckedChanged += new System.EventHandler(this._cb_auto_take_CheckedChanged);
       // 
       // _btn_take_image
       // 
-      this._btn_take_image.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this._btn_take_image.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
       this._btn_take_image.BackColor = System.Drawing.Color.White;
       this._btn_take_image.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
       this._btn_take_image.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -89,34 +75,44 @@
       this._btn_take_image.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
       this._btn_take_image.Image = ((System.Drawing.Image)(resources.GetObject("_btn_take_image.Image")));
       this._btn_take_image.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-      this._btn_take_image.Location = new System.Drawing.Point(45, 12);
+      this._btn_take_image.Location = new System.Drawing.Point(70, 50);
       this._btn_take_image.Name = "_btn_take_image";
       this._btn_take_image.Size = new System.Drawing.Size(300, 60);
-      this._btn_take_image.TabIndex = 0;
+      this._btn_take_image.TabIndex = 8;
       this._btn_take_image.Text = "Take Image";
       this._btn_take_image.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
       this._btn_take_image.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
       this._btn_take_image.UseVisualStyleBackColor = true;
       this._btn_take_image.Click += new System.EventHandler(this._btn_take_image_Click);
       // 
-      // parsleyButton2
+      // _btn_calibrate
       // 
-      this.parsleyButton2.Anchor = System.Windows.Forms.AnchorStyles.None;
-      this.parsleyButton2.BackColor = System.Drawing.Color.White;
-      this.parsleyButton2.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
-      this.parsleyButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-      this.parsleyButton2.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.parsleyButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-      this.parsleyButton2.Image = ((System.Drawing.Image)(resources.GetObject("parsleyButton2.Image")));
-      this.parsleyButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-      this.parsleyButton2.Location = new System.Drawing.Point(45, 268);
-      this.parsleyButton2.Name = "parsleyButton2";
-      this.parsleyButton2.Size = new System.Drawing.Size(300, 60);
-      this.parsleyButton2.TabIndex = 2;
-      this.parsleyButton2.Text = "Calibrate";
-      this.parsleyButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-      this.parsleyButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-      this.parsleyButton2.UseVisualStyleBackColor = true;
+      this._btn_calibrate.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this._btn_calibrate.BackColor = System.Drawing.Color.White;
+      this._btn_calibrate.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+      this._btn_calibrate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this._btn_calibrate.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this._btn_calibrate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      this._btn_calibrate.Image = ((System.Drawing.Image)(resources.GetObject("_btn_calibrate.Image")));
+      this._btn_calibrate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this._btn_calibrate.Location = new System.Drawing.Point(70, 282);
+      this._btn_calibrate.Name = "_btn_calibrate";
+      this._btn_calibrate.Size = new System.Drawing.Size(300, 60);
+      this._btn_calibrate.TabIndex = 2;
+      this._btn_calibrate.Text = "Calibrate";
+      this._btn_calibrate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this._btn_calibrate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+      this._btn_calibrate.UseVisualStyleBackColor = true;
+      this._btn_calibrate.Click += new System.EventHandler(this._btn_calibrate_Click);
+      // 
+      // _lbl_info
+      // 
+      this._lbl_info.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this._lbl_info.Location = new System.Drawing.Point(70, 179);
+      this._lbl_info.Name = "_lbl_info";
+      this._lbl_info.Size = new System.Drawing.Size(300, 38);
+      this._lbl_info.TabIndex = 4;
+      this._lbl_info.Text = "Start the calibration process by taking images";
       // 
       // IntrinsicCalibrationSlide
       // 
@@ -124,8 +120,9 @@
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.Controls.Add(this.tableLayoutPanel1);
       this.Name = "IntrinsicCalibrationSlide";
-      this.Size = new System.Drawing.Size(391, 341);
+      this.Size = new System.Drawing.Size(441, 370);
       this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
       this.ResumeLayout(false);
 
     }
@@ -133,9 +130,9 @@
     #endregion
 
     private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-    private Parsley.UI.ParsleyButton parsleyButton2;
-    private Parsley.UI.ParsleyButton parsleyButton1;
-    private Parsley.UI.ParsleyButton _btn_take_image;
+    private Parsley.UI.ParsleyButton _btn_calibrate;
     private Parsley.UI.GrowLabel _lbl_info;
+    private Parsley.UI.ParsleyButton _btn_take_image;
+    private System.Windows.Forms.CheckBox _cb_auto_take;
   }
 }
--- Parsley/IntrinsicCalibrationSlide.cs
+++ Parsley/IntrinsicCalibrationSlide.cs
@@ -14,13 +14,68 @@ namespace Parsley {
   public partial class IntrinsicCalibrationSlide : FrameGrabberSlide {
     private Core.CalibrationPattern _pattern;
     private Core.IntrinsicCalibration _ic;
+    private Core.ExtrinsicCalibration _ec; // used for illustration of coordinate frame only.
     private bool _take_image_request;
+    private BackgroundWorker _bw_calibrator;
+    private Timer _timer_auto;
 
 
     public IntrinsicCalibrationSlide(Core.FrameGrabber fg, Core.CalibrationPattern pattern) : base(fg) {
       InitializeComponent();
       _pattern = pattern;
       _ic = new Parsley.Core.IntrinsicCalibration(pattern.ObjectPoints, fg.Camera.FrameSize);
+      
+
+      _timer_auto = new Timer();
+      _timer_auto.Interval = 3000;
+      _timer_auto.Tick += new EventHandler(_timer_auto_Tick);
+
+      _bw_calibrator = new BackgroundWorker();
+      _bw_calibrator.DoWork += new DoWorkEventHandler(_bw_calibrator_DoWork);
+      _bw_calibrator.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_bw_calibrator_RunWorkerCompleted);
+    }
+
+
+    public override void OnSlidingIn() {
+      base.OnSlidingIn();
+      _timer_auto.Enabled = false;
+      _cb_auto_take.Checked = false;
+      _btn_calibrate.Enabled = false;
+      _btn_take_image.Enabled = true;
+      if (FrameGrabber.Camera.HasIntrinsics) {
+        _lbl_info.Text = "The camera already has a calibration. You can restart the calibration process by taking images. You need at least 3 images to proceed.";
+      } else {
+        _lbl_info.Text = "You can restart the calibration process by taking images. You need at least 3 images to proceed.";
+      }
+      
+      _ic.ClearViews();
+    }
+
+    public override void OnSlidingOut(CancelEventArgs args) {
+      if (_bw_calibrator.IsBusy) {
+        args.Cancel = true;
+      } else {
+        _timer_auto.Enabled = false;
+        base.OnSlidingOut(args);
+      }
+    }
+
+    void _timer_auto_Tick(object sender, EventArgs e) {
+      _take_image_request = true;
+    }
+
+    void _bw_calibrator_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+      _ic.ClearViews();
+      _btn_calibrate.Enabled = false;
+      _btn_take_image.Enabled = true;
+      _cb_auto_take.Enabled = true;
+      _cb_auto_take.Checked = false;
+      _lbl_info.Text = "Calibration succeeded!";
+    }
+
+    void _bw_calibrator_DoWork(object sender, DoWorkEventArgs e) {
+      this.FrameGrabber.Camera.Intrinsics = _ic.Calibrate();
+      _ec = new Parsley.Core.ExtrinsicCalibration(_pattern.ObjectPoints, FrameGrabber.Camera.Intrinsics);
     }
 
     /// <summary>
@@ -36,7 +91,25 @@ namespace Parsley {
       gray._EqualizeHist();
       _pattern.FindPattern(gray);
       this.HandleTakeImageRequest();
+      this.DrawCoordinateFrame(img);
       _pattern.DrawPattern(img, _pattern.ImagePoints, _pattern.PatternFound);
+    }
+
+    void DrawCoordinateFrame(Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> img) {
+      if (_ec != null && _pattern.PatternFound) {
+        Emgu.CV.ExtrinsicCameraParameters ecp = _ec.Calibrate(_pattern.ImagePoints);
+
+        System.Drawing.PointF[] coords = Emgu.CV.CameraCalibration.ProjectPoints(
+            new MCvPoint3D32f[] { 
+              new MCvPoint3D32f(0, 0, 0),
+              new MCvPoint3D32f(50, 0, 0),
+              new MCvPoint3D32f(0, 50, 0)
+            },
+            ecp, FrameGrabber.Camera.Intrinsics);
+
+        img.Draw(new LineSegment2DF(coords[0], coords[1]), new Bgr(System.Drawing.Color.Red), 2);
+        img.Draw(new LineSegment2DF(coords[0], coords[2]), new Bgr(System.Drawing.Color.Green), 2);
+      }
     }
 
     void HandleTakeImageRequest() {
@@ -44,15 +117,34 @@ namespace Parsley {
         if (_pattern.PatternFound) {
           _ic.AddView(_pattern.ImagePoints);
           this.Invoke((MethodInvoker)delegate {
-            _lbl_info.Text = String.Format("You have successfully acquired #{0} calibration images", _ic.Views.Count);
+            _lbl_info.Text = String.Format("You have successfully acquired {0} calibration image(s)", _ic.Views.Count);
+            _btn_calibrate.Enabled = _ic.Views.Count > 2 && !_cb_auto_take.Checked;
           });
         }
-        _take_image_request = false;
       }
+      _take_image_request = false;
     }
 
     private void _btn_take_image_Click(object sender, EventArgs e) {
       _take_image_request = true;
+    }
+
+    private void _btn_calibrate_Click(object sender, EventArgs e) {
+      _take_image_request = false;
+      _timer_auto.Enabled = false;
+      _cb_auto_take.Enabled = false;
+      _btn_take_image.Enabled = false;
+      _btn_calibrate.Enabled = false;
+      _bw_calibrator.RunWorkerAsync();
+    }
+
+    private void _cb_auto_take_CheckedChanged(object sender, EventArgs e) {
+      _btn_take_image.Enabled = !_cb_auto_take.Checked;
+      _btn_calibrate.Enabled = !_cb_auto_take.Checked && _ic.Views.Count > 2;
+      if (_cb_auto_take.Checked) {
+        _lbl_info.Text = "Auto-taking calibration images every three seconds.";
+      }
+      _timer_auto.Enabled = _cb_auto_take.Checked;
     }
   }
 }
