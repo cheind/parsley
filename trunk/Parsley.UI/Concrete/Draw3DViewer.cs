fb918fa272adb1aa2ee50b3347539aba3830bb8f
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-01-21 09:19:52 +0000

    initial commit of 3d visualization form

---
+++ trunk/Parsley.UI/Concrete/Draw3DViewer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parsley.UI.Concrete {
+  public partial class Draw3DViewer : AspectRatioForm {
+    private Core.RenderLoop _rl;
+
+    public Draw3DViewer() {
+      InitializeComponent();
+      _rl = new Parsley.Core.RenderLoop(_render_target);
+    }
+
+    /// <summary>
+    /// Access the Core.RenderLoop
+    /// </summary>
+    public Core.RenderLoop RenderLoop {
+      get { return _rl; }
+    }
+
+    /// <summary>
+    /// Access the Draw3D.Viewer
+    /// </summary>
+    public Draw3D.Viewer Viewer {
+      get { return _rl.Viewer; }
+    }
+  }
+}
