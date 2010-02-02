ae437d13608087cd4998482d80870a5603cb8a95
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-02-02 20:57:05 +0000

    plane can be constructed from extrinsic referenceplane holds deviation info (probably unnecessary) ExtrinsicCalibration can calculate the error of Plane WeightedAverageLLE stops after first gauss (note: when camera and laser do not share the same view, then there can be situations where in one row multiple laserlines are present)

--- Parsley.Core/ExtrinsicCalibration.cs
+++ Parsley.Core/ExtrinsicCalibration.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Emgu.CV.Structure;
 using MathNet.Numerics.LinearAlgebra;
+using Parsley.Core.Extensions;
 
 namespace Parsley.Core {
 
@@ -41,5 +42,58 @@ namespace Parsley.Core {
        image_points,
        _intrinsics);
     }
+
+    public static void CalibrationError(
+      Emgu.CV.ExtrinsicCameraParameters ecp,
+      Emgu.CV.IntrinsicCameraParameters icp,
+      System.Drawing.PointF[] image_points,
+      Vector[] reference_points,
+      out double[] deviations,
+      out Vector[] isect_points) 
+    {
+      // Shoot rays through image points,
+      // intersect with plane defined by extrinsic
+      // and measure distance to reference points
+      Matrix inv_ecp = Matrix.Identity(4, 4);
+      inv_ecp.SetMatrix(0, 2, 0, 3, ecp.ExtrinsicMatrix.ToParsley());
+      inv_ecp = inv_ecp.Inverse();
+
+      Ray[] rays = Ray.EyeRays(icp, image_points);
+      Plane p = new Plane(ecp);
+      isect_points = new Vector[rays.Length];
+
+      deviations = new double[rays.Length];
+      for (int i = 0; i < rays.Length; ++i) {
+        double t;
+        Intersection.RayPlane(rays[i], p, out t);
+        Vector isect = rays[i].At(t);
+        Vector x = new Vector(new double[]{isect[0],isect[1],isect[2],1});
+        x = (inv_ecp * x.ToColumnMatrix()).GetColumnVector(0);
+        Vector final = new Vector(new double[]{x[0], x[1], x[2]});
+        isect_points[i] = final;
+        deviations[i] = (final - reference_points[i]).Norm();
+      }
+    }
+
+    public static void VisualizeError(
+      Emgu.CV.Image<Bgr, Byte> img,
+      Emgu.CV.ExtrinsicCameraParameters ecp,
+      Emgu.CV.IntrinsicCameraParameters icp,
+      double[] deviations,
+      Vector[] isect_points) 
+    {
+      Bgr bgr = new Bgr(System.Drawing.Color.Green);
+      MCvFont f = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_PLAIN, 0.8, 0.8);
+      foreach (Vector p in isect_points) {
+        
+        System.Drawing.PointF[] coords = Emgu.CV.CameraCalibration.ProjectPoints(
+          new MCvPoint3D32f[] { p.ToEmguF() },
+          ecp, icp
+        );
+        img.Draw(new CircleF(coords[0], 1), bgr, 1);
+      }
+    }
+
+
   }
 }
