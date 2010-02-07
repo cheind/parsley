bf12788abf376596fef2acd117395f730ec79fac
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-02-07 18:58:25 +0000

    added reference planes and extrinsics

--- trunk/Parsley.Core.BuildingBlocks/World.cs
+++ trunk/Parsley.Core.BuildingBlocks/World.cs
@@ -17,6 +17,8 @@ namespace Parsley.Core.BuildingBlocks {
     private BuildingBlocks.Laser _laser;
     private Core.CalibrationPattern _intrinsic_pattern;
     private Core.CalibrationPattern _extrinsic_pattern;
+    private List<Core.Plane> _reference_planes;
+    private List<Emgu.CV.ExtrinsicCameraParameters> _extrinsics;
 
     /// <summary>
     /// Default constructor of entities
@@ -26,8 +28,13 @@ namespace Parsley.Core.BuildingBlocks {
       _laser = new Laser();
       _intrinsic_pattern = new Core.CheckerBoard(9, 6, 25.0f);
       _extrinsic_pattern = new Core.CheckerBoard(9, 6, 10.0f);
+      _reference_planes = new List<Plane>();
+      _extrinsics = new List<Emgu.CV.ExtrinsicCameraParameters>();
     }
 
+    /// <summary>
+    /// Get/set the camera
+    /// </summary>
     [Description("Adjust camera settings")]
     [TypeConverter(typeof(ExpandableObjectConverter))]
     public Camera Camera {
@@ -35,11 +42,30 @@ namespace Parsley.Core.BuildingBlocks {
       set { _camera = value; }
     }
 
+    /// <summary>
+    /// Get/set laser and laser extraction algorithms
+    /// </summary>
     [Description("Adjust laser settings")]
     [TypeConverter(typeof(ExpandableObjectConverter))]
     public Laser Laser {
       get { return _laser; }
       set { _laser = value; }
+    }
+
+    /// <summary>
+    /// Get the recorded reference planes
+    /// </summary>
+    [Browsable(false)]
+    public List<Plane> ReferencePlanes {
+      get { return _reference_planes; }
+    }
+
+    /// <summary>
+    /// Get the recorded reference planes
+    /// </summary>
+    [Browsable(false)]
+    public List<Emgu.CV.ExtrinsicCameraParameters> Extrinsics {
+      get { return _extrinsics; }
     }
 
     /// <summary>
@@ -63,8 +89,5 @@ namespace Parsley.Core.BuildingBlocks {
       get { return _extrinsic_pattern; }
       set { _extrinsic_pattern = value; }
     }
-    
-
-
   }
 }
