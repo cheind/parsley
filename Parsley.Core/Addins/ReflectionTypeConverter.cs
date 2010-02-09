c2593ef3e10e857b641473641c42cb0614d8a7f6
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-02-09 08:04:00 +0000

    mvoed ReflectionTypeConverter to addins namespace

--- Parsley.Core/Addins/ReflectionTypeConverter.cs
+++ Parsley.Core/Addins/ReflectionTypeConverter.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 
-namespace Parsley.Core {
+namespace Parsley.Core.Addins {
 
   /// <summary>
   /// Used as TypeConverter in a PropertyGrid to populate drop-down with
