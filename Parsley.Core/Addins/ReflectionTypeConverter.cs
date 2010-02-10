a08c53ff5266ef403af7e878c8c0ea9a9b081c4a
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-02-10 14:15:23 +0000

    fixed

--- Parsley.Core/Addins/ReflectionTypeConverter.cs
+++ Parsley.Core/Addins/ReflectionTypeConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
+using System.Globalization;
 
 namespace Parsley.Core.Addins {
 
@@ -15,34 +16,81 @@ namespace Parsley.Core.Addins {
   /// </summary>
   public class ReflectionTypeConverter : ExpandableObjectConverter {
     private Type _type_of;
-    private Dictionary<string, Core.Addins.AddinInfo> _type_dict;
+    private Dictionary<string, Core.Addins.AddinInfo> _addin_dict;
 
+    /// <summary>
+    /// Initialize with type to reflect
+    /// </summary>
+    /// <param name="type_of"></param>
     public ReflectionTypeConverter(Type type_of) {
       _type_of = type_of;
     }
 
-    public override object ConvertFrom(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value) {
-      if (value.GetType() == typeof(string)) {
-        return Addins.AddinStore.CreateInstance(_type_dict[(string)value]);
-      } else
+    /// <summary>
+    /// Converts the given value to the type of this converter.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="culture"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public override object ConvertFrom(
+      System.ComponentModel.ITypeDescriptorContext context, 
+      System.Globalization.CultureInfo culture, 
+      object value) 
+    {
+      if (value is string) {
+        Addins.AddinInfo addin = _addin_dict[(string)value];
+        return Addins.AddinStore.CreateInstance(addin);
+      } else {
         return base.ConvertFrom(context, culture, value);
+      }
     }
 
+    /// <summary>
+    /// Converts the given value object to the specified type, using the arguments.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="culture"></param>
+    /// <param name="value"></param>
+    /// <param name="destType"></param>
+    /// <returns></returns>
+    public override object ConvertTo(
+           ITypeDescriptorContext context,
+           CultureInfo culture,
+           object value,
+           Type destType) 
+    {
+      if (destType == typeof(string)) {
+        return value.GetType().FullName;
+      }
+      return base.ConvertTo(context, culture, value, destType);
+    }
+
+
+    /// <summary>
+    /// Returns whether this object supports a standard set of values that can be picked from a list.
+    /// </summary>
     public override bool GetStandardValuesSupported(System.ComponentModel.ITypeDescriptorContext context) {
       return true;
     }
 
+    /// <summary>
+    /// Returns whether this object supports a standard set of values that can be picked from a list, using the specified context.
+    /// </summary>
     public override System.ComponentModel.TypeConverter.StandardValuesCollection GetStandardValues(System.ComponentModel.ITypeDescriptorContext context) {
       IEnumerable<Addins.AddinInfo> t = Addins.AddinStore.FindAddins(_type_of, ai => ai.DefaultConstructible);
-      _type_dict = t.ToDictionary(ai => ai.FullName);
-      return new StandardValuesCollection(t.Select(ai => ai.FullName).ToArray());
+      _addin_dict = t.ToDictionary(ai => ai.FullName);
+      return new StandardValuesCollection(t.ToArray());
     }
 
+    /// <summary>
+    /// Returns whether this converter can convert an object of the given type to the type of this converter.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="sourceType"></param>
+    /// <returns></returns>
     public override bool CanConvertFrom(System.ComponentModel.ITypeDescriptorContext context, System.Type sourceType) {
-      if (sourceType == typeof(string))
-        return true;
-      else
-        return base.CanConvertFrom(context, sourceType);
+      return sourceType == typeof(string);
     }
   }
 }
