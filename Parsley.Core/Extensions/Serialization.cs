08755c8d987128df74864cb822e8bb4580c01e07
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-02-10 20:21:09 +0000

    added extension to ease custom serialization

---
+++ Parsley.Core/Extensions/Serialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Parsley.Core.Extensions {
+  public static class Serialization {
+
+    public static T GetValue<T>(this SerializationInfo info, string name) {
+      return (T)info.GetValue(name, typeof(T));
+    }
+
+  }
+}
