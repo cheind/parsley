7bb3083271b0721459e7d3c92be2c438cb7b4cc8
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-02-13 14:31:05 +0000

    removed world. moved content to setup. ScanWorkflow receives setup at each Process invocation.

--- trunk/Parsley.Core.BuildingBlocks/ScanWorkflow.cs
+++ trunk/Parsley.Core.BuildingBlocks/ScanWorkflow.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using Emgu.CV.Structure;
 using MathNet.Numerics.LinearAlgebra;
 using System.Runtime.Serialization;
+using System.Drawing;
 
 namespace Parsley.Core.BuildingBlocks {
   
@@ -15,16 +16,12 @@ namespace Parsley.Core.BuildingBlocks {
   /// </summary>
   [Serializable]
   public class ScanWorkflow : IDeserializationCallback {
-    [NonSerialized]
-    private World _world;
     private System.Drawing.Rectangle _roi;
     private Core.ILaserLineAlgorithm _line_algorithm;
     private Core.ILaserPlaneAlgorithm _plane_algorithm;
     private Core.ILaserLineFilterAlgorithm _line_filter;
     private Core.IPointPerPixelAccumulator _point_accum;
     private Core.ILaserPlaneFilterAlgorithm _plane_filter;
-    [NonSerialized]
-    private Core.LaserPlaneFilterAlgorithmContext _context;
 
     public ScanWorkflow() {
       _line_algorithm = new LaserLineAlgorithms.WeightedAverage(220);
@@ -32,37 +29,17 @@ namespace Parsley.Core.BuildingBlocks {
       _plane_algorithm = new LaserPlaneAlgorithms.PlaneRansac();
       _plane_filter = new LaserPlaneAlgorithms.FilterByCameraPlaneAngle();
       _point_accum = new MedianPointAccumulator();
-      _context = new LaserPlaneFilterAlgorithmContext();
     }
 
     public void OnDeserialization(object sender) {
-      _context = new LaserPlaneFilterAlgorithmContext();
       _point_accum.Size = _roi.Size;
     }
 
-    /// <summary>
-    /// Get/Set the world reference
-    /// </summary>
-    [Browsable(false)]
-    public World World {
-      set { 
-        _world = value;
-        _context.Intrinsics = World.Camera.Intrinsics;
-        _context.ReferencePlanes = World.ReferencePlanes.ToArray();
-      }
-      get { return _world; }
-    }
-
-    /// <summary>
-    /// Access the scanning region of interest
-    /// </summary>
-    [DefaultValue("0; 0; 0; 0")]
-    public System.Drawing.Rectangle ROI {
+    public Rectangle ROI{
       get { return _roi; }
       set { 
         _roi = value;
-        _point_accum.Size = _roi.Size;
-        _context.ROI = value;
+        _point_accum.Size = value.Size;
       }
     }
 
@@ -162,40 +139,46 @@ namespace Parsley.Core.BuildingBlocks {
     /// <summary>
     /// Process image
     /// </summary>
+    /// <param name="s">Setup</param>
     /// <param name="image">Image</param>
     /// <param name="points">Found points</param>
     /// <param name="pixels">Corresponding pixels for each point</param>
     /// <returns>True if successful, false otherwise</returns>
-    public bool Process(Emgu.CV.Image<Bgr, byte> image, out Vector[] points, out System.Drawing.Point[] pixels) {
+    public bool Process(Setup s, Emgu.CV.Image<Bgr, byte> image, out Vector[] points, out System.Drawing.Point[] pixels) {
       points = null;
       pixels = null;
 
+      Core.LaserPlaneFilterAlgorithmContext context = new LaserPlaneFilterAlgorithmContext();
+      context.Intrinsics = s.Camera.Intrinsics;
+      context.ROI = _roi;
+      context.ReferencePlanes = s.ReferencePlanes.ToArray();
+
 
       // 1. Update context
-      _context.Image = image;
-      _context.ChannelImage = image[(int)World.Laser.Color];
+      context.Image = image;
+      context.ChannelImage = image[(int)s.Laser.Color];
       
       // 2. Extract laser line
       System.Drawing.PointF[] laser_points;
-      if (!_line_algorithm.FindLaserLine(_context, out laser_points)) return false;
-      _context.LaserPoints = laser_points;
+      if (!_line_algorithm.FindLaserLine(context, out laser_points)) return false;
+      context.LaserPoints = laser_points;
 
       // 3. Filter laser points
       System.Drawing.PointF[] filtered_laser_points;
-      if (!_line_filter.FilterLaserLine(_context, out filtered_laser_points)) return false;
-      _context.ValidLaserPoints = filtered_laser_points.Where(p => p != System.Drawing.PointF.Empty).ToArray();
-      if (_context.ValidLaserPoints.Length < 3) return false;
+      if (!_line_filter.FilterLaserLine(context, out filtered_laser_points)) return false;
+      context.ValidLaserPoints = filtered_laser_points.Where(p => p != System.Drawing.PointF.Empty).ToArray();
+      if (context.ValidLaserPoints.Length < 3) return false;
 
       // 4. Detect laser plane
-      _context.EyeRays = Core.Ray.EyeRays(_context.Intrinsics, _context.ValidLaserPoints);
+      context.EyeRays = Core.Ray.EyeRays(context.Intrinsics, context.ValidLaserPoints);
 
       Core.Plane laser_plane;
-      if (!_plane_algorithm.FindLaserPlane(_context, out laser_plane)) return false;
-      _context.LaserPlane = laser_plane;
+      if (!_plane_algorithm.FindLaserPlane(context, out laser_plane)) return false;
+      context.LaserPlane = laser_plane;
 
       // 5. Filter laser plane
       Core.Plane filtered_laser_plane;
-      if (!_plane_filter.FilterLaserPlane(_context, out filtered_laser_plane)) {
+      if (!_plane_filter.FilterLaserPlane(context, out filtered_laser_plane)) {
         Console.WriteLine("filtered" + filtered_laser_plane.Normal);
         return false;
       }
@@ -203,16 +186,16 @@ namespace Parsley.Core.BuildingBlocks {
       // 6. Extract relevant points in ROI
       List<System.Drawing.Point> my_pixels = new List<System.Drawing.Point>();
       List<Vector> my_points = new List<Vector>();
-      
-      for (int i = 0; i < _context.ValidLaserPoints.Length; ++i) {
+
+      for (int i = 0; i < context.ValidLaserPoints.Length; ++i) {
         // Round to nearest pixel
         System.Drawing.Point p = new System.Drawing.Point(
-          (int)_context.ValidLaserPoints[i].X,
-          (int)_context.ValidLaserPoints[i].Y);
+          (int)context.ValidLaserPoints[i].X,
+          (int)context.ValidLaserPoints[i].Y);
 
         double t;
         if (_roi.Contains(p)) {
-          Core.Ray r = _context.EyeRays[i];
+          Core.Ray r = context.EyeRays[i];
           if (Core.Intersection.RayPlane(r, filtered_laser_plane, out t)) {
             System.Drawing.Point in_roi = Core.IndexHelper.MakeRelative(p, _roi);
             my_pixels.Add(p);
--- trunk/Parsley.Core.BuildingBlocks/Setup.cs
+++ trunk/Parsley.Core.BuildingBlocks/Setup.cs
@@ -11,7 +11,10 @@ namespace Parsley.Core.BuildingBlocks {
 
   [Serializable]
   public class Setup {
-    private World _world;
+    private BuildingBlocks.Camera _camera;
+    private BuildingBlocks.Laser _laser;
+    private List<Core.Plane> _reference_planes;
+    private List<Emgu.CV.ExtrinsicCameraParameters> _extrinsics;
     private ScanWorkflow _wf;
     private Core.CalibrationPattern _intrinsic_pattern;
     private Core.CalibrationPattern _extrinsic_pattern;
@@ -20,27 +23,13 @@ namespace Parsley.Core.BuildingBlocks {
     /// Construct default setup
     /// </summary>
     public Setup() {
-      _world = new World();
       _wf = new ScanWorkflow();
       _intrinsic_pattern = new Core.CalibrationPatterns.CheckerBoard(9, 6, 25.0f);
       _extrinsic_pattern = new Core.CalibrationPatterns.CheckerBoard(9, 6, 10.0f);
-    }
-
-    /// <summary>
-    /// Get/set world components
-    /// </summary>
-    [Description("Adjust settings of world components")]
-    [TypeConverter(typeof(ExpandableObjectConverter))]
-    public World World {
-      get { return _world; }
-      set { _world = value; }
-    }
-
-    /// <summary>
-    /// Used instead of DefaultValueAttribute
-    /// </summary>
-    public bool ShouldSerializeWorld() {
-      return false;
+      _camera = new Camera(0);
+      _laser = new Laser();
+      _reference_planes = new List<Plane>();
+      _extrinsics = new List<Emgu.CV.ExtrinsicCameraParameters>();
     }
 
     /// <summary>
@@ -98,6 +87,56 @@ namespace Parsley.Core.BuildingBlocks {
     /// </summary>
     public bool ShouldSerializeExtrinsicPattern() {
       return _intrinsic_pattern.GetType() != typeof(Core.CalibrationPatterns.CheckerBoard);
+    }
+
+    /// <summary>
+    /// Get/set the camera
+    /// </summary>
+    [Description("Adjust camera settings")]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public Camera Camera {
+      get { return _camera; }
+      set { _camera = value; }
+    }
+
+    /// <summary>
+    /// Used instead of DefaultValueAttribute
+    /// </summary>
+    public bool ShouldSerializeCamera() {
+      return false;
+    }
+
+    /// <summary>
+    /// Get/set laser and laser extraction algorithms
+    /// </summary>
+    [Description("Adjust laser settings")]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public Laser Laser {
+      get { return _laser; }
+      set { _laser = value; }
+    }
+
+    /// <summary>
+    /// Used instead of DefaultValueAttribute
+    /// </summary>
+    public bool ShouldSerializeLaser() {
+      return false;
+    }
+
+    /// <summary>
+    /// Get the recorded reference planes
+    /// </summary>
+    [Browsable(false)]
+    public List<Plane> ReferencePlanes {
+      get { return _reference_planes; }
+    }
+
+    /// <summary>
+    /// Get the recorded reference planes
+    /// </summary>
+    [Browsable(false)]
+    public List<Emgu.CV.ExtrinsicCameraParameters> Extrinsics {
+      get { return _extrinsics; }
     }
 
     /// <summary>
