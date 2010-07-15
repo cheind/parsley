c9362d21a5d41aa2436c29efa525d086e72fc97a
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-07-15 15:29:03 +0000

    added propertygrid to modify algorithm settings.

--- Parsley/ImageAlgorithmTestSlide.Designer.cs
+++ Parsley/ImageAlgorithmTestSlide.Designer.cs
@@ -24,6 +24,8 @@
     /// </summary>
     private void InitializeComponent() {
       this._cmb_algorithm = new System.Windows.Forms.ComboBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this._property_grid = new System.Windows.Forms.PropertyGrid();
       this.SuspendLayout();
       // 
       // growLabel1
@@ -33,28 +35,52 @@
       // _cmb_algorithm
       // 
       this._cmb_algorithm.FormattingEnabled = true;
-      this._cmb_algorithm.Location = new System.Drawing.Point(4, 56);
+      this._cmb_algorithm.Location = new System.Drawing.Point(6, 65);
       this._cmb_algorithm.Name = "_cmb_algorithm";
       this._cmb_algorithm.Size = new System.Drawing.Size(182, 23);
       this._cmb_algorithm.TabIndex = 14;
       this._cmb_algorithm.SelectedIndexChanged += new System.EventHandler(this._cmb_algorithm_SelectedIndexChanged);
       // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(3, 47);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(133, 15);
+      this.label1.TabIndex = 15;
+      this.label1.Text = "Select Algorithm to Run";
+      // 
+      // _property_grid
+      // 
+      this._property_grid.Location = new System.Drawing.Point(6, 94);
+      this._property_grid.Name = "_property_grid";
+      this._property_grid.Size = new System.Drawing.Size(252, 253);
+      this._property_grid.TabIndex = 16;
+      this._property_grid.ToolbarVisible = false;
+      // 
       // ImageAlgorithmTestSlide
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.label1);
       this.Controls.Add(this._cmb_algorithm);
+      this.Controls.Add(this._property_grid);
       this.Name = "ImageAlgorithmTestSlide";
       this.Size = new System.Drawing.Size(550, 429);
       this.Load += new System.EventHandler(this.ImageAlgorithmTestSlide_Load);
+      this.Controls.SetChildIndex(this._property_grid, 0);
       this.Controls.SetChildIndex(this._cmb_algorithm, 0);
+      this.Controls.SetChildIndex(this.label1, 0);
       this.Controls.SetChildIndex(this.growLabel1, 0);
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.ComboBox _cmb_algorithm;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.PropertyGrid _property_grid;
   }
 }
