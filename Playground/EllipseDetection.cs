c0bdc9d729e3fc84b4a3bbfad7f9bddad6f510c7
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-07-16 16:11:12 +0000

    fixed calculation of ellipse rating.

--- Playground/EllipseDetection.cs
+++ Playground/EllipseDetection.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 
 using Emgu.CV.Structure;
 using MathNet.Numerics.LinearAlgebra;
+using Parsley.Core.Extensions;
 
 namespace Playground {
 
@@ -21,7 +22,7 @@ namespace Playground {
     public EllipseDetection() {
       _threshold = 40;
       _min_contour_count = 40;
-      _distance_threshold = 0.3;
+      _distance_threshold = 1;
     }
 
     /// <summary>
@@ -46,13 +47,11 @@ namespace Playground {
       get { return _distance_threshold; }
       set { _distance_threshold = value; }
     }
-    
+
     public void ProcessImage(Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> image) {
       Emgu.CV.Image<Gray, byte> gray = image.Convert<Gray, byte>();
       gray._ThresholdBinary(new Gray(_threshold), new Gray(255.0));
       gray._Not();
-
-      //Emgu.CV.Contour<System.Drawing.Point> c = gray.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_CODE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_LIST);
       Emgu.CV.Contour<System.Drawing.Point> c = gray.FindContours();
       List<Ellipse> ellipses = new List<Ellipse>();
       
@@ -69,21 +68,33 @@ namespace Playground {
           box.size.Width *= 0.5f;
           Ellipse final_ellipse = new Ellipse(box);
 
-          Matrix m = Matrix.Identity(3,3);
+          // Find out which dimension is bigger
           double a = final_ellipse.MCvBox2D.size.Width;
           double b = final_ellipse.MCvBox2D.size.Height;
-          double s = a / b;
-          Matrix scale = Matrix.Identity(3,3);
-          scale[0, 0] = s;
-          double angle = final_ellipse.MCvBox2D.angle / 180 * Math.PI;
-          m[0, 0] = Math.Cos(angle);
-          m[0, 1] = -Math.Sin(angle);
-          m[0,2] = final_ellipse.MCvBox2D.center.X;
-          m[1, 0] = Math.Sin(angle);
-          m[1, 1] = Math.Cos(angle);
-          m[1,2] = final_ellipse.MCvBox2D.center.Y;
+          
+          double scaling = 1.0;
+          if (a > b) {
+            scaling = b / a;
+          } else {
+            scaling = a / b;
+          }
+          
+          Matrix scale = Matrix.Identity(3, 3);
+          scale[0, 0] = scaling; // Why scale the x-axis
 
-          m = scale * m;
+          Matrix translation = Matrix.Identity(3, 3);
+          translation[0, 2] = final_ellipse.MCvBox2D.center.X;
+          translation[1, 2] = final_ellipse.MCvBox2D.center.Y;
+
+          Matrix rotation = Matrix.Identity(3, 3);
+          double angle = final_ellipse.MCvBox2D.angle / 180 * Math.PI;
+          // Rotation counterclockwise
+          rotation[0, 0] = Math.Cos(-angle);
+          rotation[0, 1] = -Math.Sin(-angle);
+          rotation[1, 0] = Math.Sin(-angle);
+          rotation[1, 1] = Math.Cos(-angle);
+
+          Matrix m = translation * rotation * scale;
           Matrix inv = m.Inverse();
           List<double> ratings = new List<double>();
           
@@ -92,37 +103,17 @@ namespace Playground {
           double height = image.Height;
           
           foreach (System.Drawing.PointF p in mypoints) {
-            Vector x = new Vector(new double[]{p.X, p.Y, 1});
-            Vector r = (inv.Multiply(x.ToColumnMatrix())).GetColumnVector(0).Normalize();
-            Vector closest = r * a;
-
-            Vector closest_in_world = (m.Multiply(closest.ToColumnMatrix())).GetColumnVector(0);
-
-
-            System.Drawing.Point mypoint = new System.Drawing.Point((int)closest_in_world[0], (int)closest_in_world[1]);
-            if (mypoint.X >= 0 && mypoint.Y >= 0 && mypoint.X < width && mypoint.Y < height) {
-              image[mypoint.Y, mypoint.X] = new Bgr(0, 0, 255);
-              image[(int)p.Y, (int)p.X] = new Bgr(0, 255, 255);
-            }
-
-            Vector distance_in_world = closest_in_world - x;
-
-            ratings.Add(distance_in_world.Norm());
+            Vector x = p.ToParsley().ToHomogeneous(1.0);
+            Vector r = (inv.Multiply(x.ToColumnMatrix())).GetColumnVector(0);
+            Vector closest = r.ToNonHomogeneous().Normalize().Scale(b);
+            Vector closest_in_world = (m.Multiply(closest.ToHomogeneous(1.0).ToColumnMatrix())).GetColumnVector(0);         
+            ratings.Add((closest_in_world - x).ToNonHomogeneous().Norm());
           }
 
-          ratings.Sort();
-          double rating;
-          if (ratings.Count % 2 == 0) {
-            rating = (ratings[ratings.Count/2] + ratings[ratings.Count/2+1]) * 0.5;
-          } else {
-            rating = ratings[(ratings.Count+1)/2];
-          }
-          //Console.WriteLine(rating);
-          if (rating < _distance_threshold) {
+          double median_distance = ratings.Median();
+          if (median_distance < _distance_threshold) {
             ellipses.Add(final_ellipse);
           }
-          
-
         }
         c = c.HNext;
       }
