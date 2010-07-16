9602361dde68d455dceb53b5da376d3bf98797eb
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-07-16 17:03:06 +0000

    refactored to use EllipseDetection.

--- branches/lizard/Playground/EllipseDetection.cs
+++ branches/lizard/Playground/EllipseDetection.cs
@@ -17,8 +17,6 @@ namespace Playground {
     private int _min_contour_count;
     private double _distance_threshold;
 
-
-
     public EllipseDetection() {
       _threshold = 40;
       _min_contour_count = 40;
@@ -52,76 +50,22 @@ namespace Playground {
       Emgu.CV.Image<Gray, byte> gray = image.Convert<Gray, byte>();
       gray._ThresholdBinary(new Gray(_threshold), new Gray(255.0));
       gray._Not();
-      Emgu.CV.Contour<System.Drawing.Point> c = gray.FindContours();
-      List<Ellipse> ellipses = new List<Ellipse>();
-      
-      while (c != null) {
-        if (c.Count() >= _min_contour_count) {
-          System.Drawing.PointF[] mypoints = Array.ConvertAll(
-            c.ToArray<System.Drawing.Point>(), 
-            value => new System.Drawing.PointF(value.X, value.Y)
-          );
 
-          Ellipse e = Emgu.CV.PointCollection.EllipseLeastSquareFitting(mypoints);
-          MCvBox2D box = e.MCvBox2D;
-          box.size.Height *= 0.5f;
-          box.size.Width *= 0.5f;
-          Ellipse final_ellipse = new Ellipse(box);
+      Parsley.Core.EllipseDetector ed = new Parsley.Core.EllipseDetector();
+      ed.MinimumContourCount = _min_contour_count;
 
-          // Find out which dimension is bigger
-          double a = final_ellipse.MCvBox2D.size.Width;
-          double b = final_ellipse.MCvBox2D.size.Height;
-          
-          double scaling = 1.0;
-          if (a > b) {
-            scaling = b / a;
-          } else {
-            scaling = a / b;
-          }
-          
-          Matrix scale = Matrix.Identity(3, 3);
-          scale[0, 0] = scaling; // Why scale the x-axis
+      List < Parsley.Core.DetectedEllipse > ellipses = 
+        new List<Parsley.Core.DetectedEllipse>(ed.DetectEllipses(gray));
 
-          Matrix translation = Matrix.Identity(3, 3);
-          translation[0, 2] = final_ellipse.MCvBox2D.center.X;
-          translation[1, 2] = final_ellipse.MCvBox2D.center.Y;
+      List < Parsley.Core.DetectedEllipse > finals = 
+        new List<Parsley.Core.DetectedEllipse>(
+          ellipses.Where(e => { return e.Rating < _distance_threshold; })
+        );
 
-          Matrix rotation = Matrix.Identity(3, 3);
-          double angle = final_ellipse.MCvBox2D.angle / 180 * Math.PI;
-          // Rotation counterclockwise
-          rotation[0, 0] = Math.Cos(-angle);
-          rotation[0, 1] = -Math.Sin(-angle);
-          rotation[1, 0] = Math.Sin(-angle);
-          rotation[1, 1] = Math.Cos(-angle);
-
-          Matrix m = translation * rotation * scale;
-          Matrix inv = m.Inverse();
-          List<double> ratings = new List<double>();
-          
-
-          double width = image.Width;
-          double height = image.Height;
-          
-          foreach (System.Drawing.PointF p in mypoints) {
-            Vector x = p.ToParsley().ToHomogeneous(1.0);
-            Vector r = (inv.Multiply(x.ToColumnMatrix())).GetColumnVector(0);
-            Vector closest = r.ToNonHomogeneous().Normalize().Scale(b);
-            Vector closest_in_world = (m.Multiply(closest.ToHomogeneous(1.0).ToColumnMatrix())).GetColumnVector(0);         
-            ratings.Add((closest_in_world - x).ToNonHomogeneous().Norm());
-          }
-
-          double median_distance = ratings.Median();
-          if (median_distance < _distance_threshold) {
-            ellipses.Add(final_ellipse);
-          }
-        }
-        c = c.HNext;
-      }
-      
-      ellipses.Sort(
+      finals.Sort(
         (a, b) => {
-          double dista = a.MCvBox2D.center.X * a.MCvBox2D.center.X + a.MCvBox2D.center.Y * a.MCvBox2D.center.Y;
-          double distb = b.MCvBox2D.center.X * b.MCvBox2D.center.X + b.MCvBox2D.center.Y * b.MCvBox2D.center.Y;
+          double dista = a.Ellipse.MCvBox2D.center.X * a.Ellipse.MCvBox2D.center.X + a.Ellipse.MCvBox2D.center.Y * a.Ellipse.MCvBox2D.center.Y;
+          double distb = b.Ellipse.MCvBox2D.center.X * b.Ellipse.MCvBox2D.center.X + b.Ellipse.MCvBox2D.center.Y * b.Ellipse.MCvBox2D.center.Y;
           return dista.CompareTo(distb);
         }
       );
@@ -129,9 +73,9 @@ namespace Playground {
       Bgr bgr = new Bgr(0, 255, 0);
       MCvFont f = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_PLAIN, 0.8, 0.8);
       int count = 1;
-      foreach (Ellipse e in ellipses) {
-        image.Draw(e, bgr, 2);
-        image.Draw(count.ToString(), ref f, new System.Drawing.Point((int)e.MCvBox2D.center.X, (int)e.MCvBox2D.center.Y), bgr);
+      foreach (Parsley.Core.DetectedEllipse e in finals) {
+        image.Draw(e.Ellipse, bgr, 2);
+        image.Draw(count.ToString(), ref f, new System.Drawing.Point((int)e.Ellipse.MCvBox2D.center.X, (int)e.Ellipse.MCvBox2D.center.Y), bgr);
         count++;
       }
     }
