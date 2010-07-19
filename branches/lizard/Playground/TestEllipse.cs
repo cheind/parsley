274dff0d0d980e5d321f35af6c2750ae0682731c
matthias.plasch <matthias.plasch@0309ed38-fbab-11de-acd2-a780054a256f>
2010-07-19 15:56:14 +0000

    added test class for printing ellipses

---
+++ branches/lizard/Playground/TestEllipse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Emgu.CV.Structure;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Playground {
+
+  [Parsley.Core.Addins.Addin]
+  public class TestEllipse : Parsley.Core.IImageAlgorithm {
+
+    public TestEllipse() { }
+    
+    public void ProcessImage(Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> image) {
+      MCvBox2D mybox = new MCvBox2D(new System.Drawing.PointF(100, 100), new System.Drawing.Size(50, 30), 110);
+      MCvBox2D mybox2 = new MCvBox2D(new System.Drawing.PointF(100, 100), new System.Drawing.Size(50, 30), 0);
+
+      image.Draw(new Ellipse(mybox), new Bgr(0,0,255), 2);
+      image.Draw(new Ellipse(mybox2), new Bgr(0, 255, 0), 2);
+    }
+  }
+}
