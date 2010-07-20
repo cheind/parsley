ea90d0689db41488bd3615da1f05702f60c8d448
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-07-20 13:44:03 +0000

    added possibility to show and auto-update content of logfile.

---
+++ Parsley/Logging/LogFileDisplay.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Parsley.Logging {
+  partial class LogFileDisplay {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogFileDisplay));
+      this._cb_auto_update = new System.Windows.Forms.CheckBox();
+      this._txt_content = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // _cb_auto_update
+      // 
+      this._cb_auto_update.AutoSize = true;
+      this._cb_auto_update.Checked = true;
+      this._cb_auto_update.CheckState = System.Windows.Forms.CheckState.Checked;
+      this._cb_auto_update.Dock = System.Windows.Forms.DockStyle.Top;
+      this._cb_auto_update.Location = new System.Drawing.Point(0, 0);
+      this._cb_auto_update.Name = "_cb_auto_update";
+      this._cb_auto_update.Size = new System.Drawing.Size(779, 17);
+      this._cb_auto_update.TabIndex = 0;
+      this._cb_auto_update.Text = "Auto Update Content";
+      this._cb_auto_update.UseVisualStyleBackColor = true;
+      this._cb_auto_update.CheckedChanged += new System.EventHandler(this._cb_auto_update_CheckedChanged);
+      // 
+      // _txt_content
+      // 
+      this._txt_content.Dock = System.Windows.Forms.DockStyle.Fill;
+      this._txt_content.Location = new System.Drawing.Point(0, 17);
+      this._txt_content.Multiline = true;
+      this._txt_content.Name = "_txt_content";
+      this._txt_content.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this._txt_content.Size = new System.Drawing.Size(779, 273);
+      this._txt_content.TabIndex = 1;
+      // 
+      // LogFileDisplay
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(779, 290);
+      this.Controls.Add(this._txt_content);
+      this.Controls.Add(this._cb_auto_update);
+      this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.Name = "LogFileDisplay";
+      this.Text = "Log File Content";
+      this.Shown += new System.EventHandler(this.LogFileDisplay_Shown);
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LogFileDisplay_FormClosing);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.CheckBox _cb_auto_update;
+    private System.Windows.Forms.TextBox _txt_content;
+  }
+}
---
+++ Parsley/Logging/LogFileDisplay.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Parsley.Logging {
+  public partial class LogFileDisplay : Form {
+    FileSystemWatcher _watcher;
+    string _full_path;
+
+    public LogFileDisplay() {
+      InitializeComponent();
+      _watcher = new FileSystemWatcher();
+      _watcher.Path = Path.GetDirectoryName(Application.ExecutablePath);
+      _watcher.Filter = "Parsley.log.txt";
+      _full_path = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Parsley.log.txt");
+      _watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.Size | NotifyFilters.LastWrite;
+      _watcher.Changed += new FileSystemEventHandler(_watcher_Changed);
+      _watcher.Created += new FileSystemEventHandler(_watcher_Created);
+      _watcher.EnableRaisingEvents = true;
+    }
+
+    private void UpdateContent() {
+      using (TextReader tr = new StreamReader(_full_path)) {
+        _txt_content.Text = tr.ReadToEnd();
+        _txt_content.SelectionStart = _txt_content.Text.Length;
+        _txt_content.ScrollToCaret();
+        _txt_content.Refresh();
+      }
+    }
+
+    void _watcher_Created(object sender, FileSystemEventArgs e) {
+      if (_txt_content.InvokeRequired) {
+        _txt_content.BeginInvoke(new MethodInvoker(delegate { UpdateContent(); }));
+      }
+    }
+
+    void _watcher_Changed(object sender, FileSystemEventArgs e) {
+      if (_txt_content.InvokeRequired) {
+        _txt_content.BeginInvoke(new MethodInvoker(delegate { UpdateContent(); }));
+      }
+    }
+
+    private void _cb_auto_update_CheckedChanged(object sender, EventArgs e) {
+      _watcher.EnableRaisingEvents = _cb_auto_update.Checked;
+      if (_cb_auto_update.Checked) {
+        UpdateContent();
+      }
+    }
+
+    private void LogFileDisplay_FormClosing(object sender, FormClosingEventArgs e) {
+      _watcher.EnableRaisingEvents = false;
+    }
+
+    private void LogFileDisplay_Shown(object sender, EventArgs e) {
+      UpdateContent();
+    }
+  }
+}
