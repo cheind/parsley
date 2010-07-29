88075370533c65093a2a1d56c25cbb767191bdce
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-07-29 06:01:53 +0000

    added new type editor to load extrinsics from a file. this eases assigning extrinsics to properties in the property grid.

---
+++ trunk/Parsley.Core.BuildingBlocks/ExtrinsicTypeEditor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing.Design;
+using System.Windows.Forms.Design;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+
+namespace Parsley.Core.BuildingBlocks {
+
+  /// <summary>
+  /// Type editor to load extrinsics from file and assign them to properties.
+  /// </summary>
+  [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+  public class ExtrinsicTypeEditor : UITypeEditor {
+
+    public ExtrinsicTypeEditor() {
+    }
+
+    public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context) {
+      // Indicates that this editor can display a Form-based interface.
+      return UITypeEditorEditStyle.Modal;
+    }
+
+    public override object EditValue(
+        ITypeDescriptorContext context,
+        IServiceProvider provider,
+        object value) {
+
+      if (context == null) {
+        return null;
+      }
+
+      
+      using (OpenFileDialog ofd = new OpenFileDialog()) {
+        ofd.Filter = "Extrinsic Files|*.ecp";
+        if (ofd.ShowDialog() == DialogResult.OK) {
+          Emgu.CV.ExtrinsicCameraParameters ecp = null;
+          using (Stream s = File.Open(ofd.FileName, FileMode.Open)) {
+            if (s != null) {
+              IFormatter formatter = new BinaryFormatter();
+              ecp = formatter.Deserialize(s) as Emgu.CV.ExtrinsicCameraParameters;
+              s.Close();
+            }
+          }
+          return ecp;
+        } else {
+          return value;
+        }
+      } 
+    }
+  }
+
+}
--- trunk/Parsley.Core.BuildingBlocks/ReferenceBody.cs
+++ trunk/Parsley.Core.BuildingBlocks/ReferenceBody.cs
@@ -13,15 +13,15 @@ namespace Parsley.Core.BuildingBlocks {
   [Serializable]
   public class ReferenceBody {
     List<Plane> _planes;
-    List<string> _plane_filenames;
+    List<Emgu.CV.ExtrinsicCameraParameters> _ecps;
 
     public ReferenceBody() {
       _planes = new List<Plane>();
       _planes.Add(null);
       _planes.Add(null);
-      _plane_filenames = new List<string>();
-      _plane_filenames.Add("not-set");
-      _plane_filenames.Add("not-set");
+      _ecps = new List<Emgu.CV.ExtrinsicCameraParameters>();
+      _ecps.Add(null);
+      _ecps.Add(null);
     }
 
     /// <summary>
@@ -32,54 +32,31 @@ namespace Parsley.Core.BuildingBlocks {
       get { return _planes; }
     }
 
-    [Editor(typeof(ExtrinsicsFileNameEditor),
+    [Editor(typeof(ExtrinsicTypeEditor),
             typeof(System.Drawing.Design.UITypeEditor))]
-    public string LeftPlane {
+    public Emgu.CV.ExtrinsicCameraParameters LeftPlane {
       get {
-        return _plane_filenames[0];
+        return _ecps[0];
       }
       set {
-        Emgu.CV.ExtrinsicCameraParameters ecp = LoadExtrinsics(value);
-        if (ecp != null) {
-          _plane_filenames[0] = value;
-          _planes[0] = new Plane(ecp);
+        if (value != null) {
+          _ecps[0] = value;
+          _planes[0] = new Plane(value);
         }
       }
     }
 
-    [Editor(typeof(ExtrinsicsFileNameEditor),
-        typeof(System.Drawing.Design.UITypeEditor))]
-    public string RightPlane {
+    [Editor(typeof(ExtrinsicTypeEditor),
+            typeof(System.Drawing.Design.UITypeEditor))]
+    public Emgu.CV.ExtrinsicCameraParameters RightPlane {
       get {
-        return _plane_filenames[1];
+        return _ecps[1];
       }
       set {
-        Emgu.CV.ExtrinsicCameraParameters ecp = LoadExtrinsics(value);
-        if (ecp != null) {
-          _plane_filenames[1] = value;
-          _planes[1] = new Plane(ecp);
+        if (value != null) {
+          _ecps[1] = value;
+          _planes[1] = new Plane(value);
         }
-      }
-    }
-
-    private Emgu.CV.ExtrinsicCameraParameters LoadExtrinsics(string filename) {
-      Emgu.CV.ExtrinsicCameraParameters ecp = null;
-      try {
-        using (Stream s = File.Open(filename, FileMode.Open)) {
-          if (s != null) {
-            IFormatter formatter = new BinaryFormatter();
-            ecp = formatter.Deserialize(s) as Emgu.CV.ExtrinsicCameraParameters;
-            s.Close();
-          }
-        }
-      } catch (Exception) { }
-      return ecp;
-    }
-
-    internal class ExtrinsicsFileNameEditor : System.Windows.Forms.Design.FileNameEditor {
-      protected override void InitializeDialog(OpenFileDialog openFileDialog) {
-        base.InitializeDialog(openFileDialog);
-        openFileDialog.Filter = "Extrinsic Files|*.ecp";
       }
     }
   }
