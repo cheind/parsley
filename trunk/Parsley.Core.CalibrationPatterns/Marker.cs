418d19cb0328f4cd68fd73c537557f4974db3b02
christoph.heindl <christoph.heindl@0309ed38-fbab-11de-acd2-a780054a256f>
2010-07-30 16:25:44 +0000

    added marker-based detection like ar-toolkit offers.

---
+++ trunk/Parsley.Core.CalibrationPatterns/Marker.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+
+using Emgu.CV.Structure;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using System.ComponentModel;
+using MathNet.Numerics.LinearAlgebra;
+using System.Drawing.Design;
+
+namespace Parsley.Core.CalibrationPatterns {
+
+  /// <summary>
+  /// A calibration pattern based on markers.
+  /// </summary>
+  /// <remarks>
+  /// The pattern to be detected is a user-defined square binary image that has a thick black border and 
+  /// a non-symmetric black/white interior. The marker is provided as an ordinary image. Additionally, 
+  /// the length of pattern in the real world in units of your choice (e.g millimeter) needs to be configured.
+  /// The calibration yields four correspondences (corner pixels of the marker) when successful.
+  /// 
+  /// The algorithm works by first binarizing the image into black and white. On this image the contours are extracted.
+  /// For each extracted contour an poly-line through the contour is approximated. If the poly-line has exactly four vertices,
+  /// the algorithm treats the poly-line as a possible marker border. For each marker hypothesis, the pixels contained in 
+  /// inside the  poly-line are back-projected to a square image of the same size (in pixels) as the marker image. This image 
+  /// is called 'warped image'. Since the contour points do not empose a constant ordering, there are four possibilities for 
+  /// the warped image to align with the marker image, namely: a rotation by 0°, 90°, 180°, 270°. For possible configuration 
+  /// the marker image is matched with the transformed warped image. The best one (in terms of the normed sum of squared 
+  /// pixel-intensity differences) is chosen if the error is below a predefined error. The best orientation is then used
+  /// to order the corner pixels accordingly.
+  /// </remarks>
+  [Serializable]
+  [Parsley.Core.Addins.Addin]
+  public class Marker : Core.CalibrationPattern {
+    private Image<Bgr, byte> _marker;
+    private Image<Gray, byte> _binary_marker, _warped, _tmp;
+    private int _marker_size;
+    private double _marker_length, _max_error_normed;
+    private int _binary_threshold;
+    private MemStorage _contour_storage;
+    private object _sync;
+    Matrix<double> _warp_matrix;
+    PointF[] _warp_dest;
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public Marker() {
+      _marker = null;
+      _binary_marker = null;
+      _warped = null;
+      _tmp = null;
+      _marker_size = 0;
+      _marker_length = 50;
+      _max_error_normed = 0.4;
+      _binary_threshold = 60;
+      _warp_matrix = new Matrix<double>(3, 3);
+      _contour_storage = new MemStorage();
+      _sync = new object();
+      this.ObjectPoints = UpdateObjectPoints();
+    }
+
+    /// <summary>
+    /// Access the marker image used as pattern
+    /// </summary>
+    [Description("Get/Set the marker image used as pattern")]
+    [Editor(typeof(Core.ImageTypeEditor), typeof(UITypeEditor))]
+    public Image<Bgr, byte> MarkerImage {
+      get { return _marker; }
+      set {
+        _marker = value; UpdateMarker();
+      }
+    }
+
+    /// <summary>
+    /// Set the threshold for blackness, used to binarize the input image.
+    /// All values less-than or equal to the treshold are made black, all others
+    /// white.
+    /// </summary>
+    [Description("Set the threshold for blackness, used to binarize the input image." +
+                 " All values less-than or equal to the treshold are made black, all others white")]
+    public int BinaryThreshold {
+      get { return _binary_threshold; }
+      set { _binary_threshold = value; }
+    }
+
+    /// <summary>
+    /// Get/Set the length of the marker
+    /// </summary>
+    [Description("Length of marker")]
+    public double MarkerLength {
+      get { return _marker_length; }
+      set { _marker_length = value; this.ObjectPoints = UpdateObjectPoints(); }
+    }
+
+    /// <summary>
+    /// Get/Set the maximum allowed error for pattern detection.
+    /// </summary>
+    [Description("Get/Set the maximum allowed error for pattern detection.")]
+    public double MaximumErrorNormed {
+      get { return _max_error_normed; }
+      set { _max_error_normed = value; }
+    }
+
+    /// <summary>
+    /// Update object reference points.
+    /// </summary>
+    /// <returns></returns>
+    private Vector[] UpdateObjectPoints() {
+
+      // 0   1
+      // +---+
+      // |   |
+      // +---+
+      // 2   3
+      return new Vector[] {
+        new Vector(new double[]{0.0, 0.0, 0.0}),
+        new Vector(new double[]{_marker_length, 0.0, 0.0}),
+        new Vector(new double[]{_marker_length, _marker_length, 0.0}),
+        new Vector(new double[]{0.0, _marker_length, 0.0})
+      };
+    }
+
+    /// <summary>
+    /// Update associated marker structures
+    /// </summary>
+    /// <param name="marker_size"></param>
+    private void UpdateMarker() {
+      if (_marker == null)
+        return;
+      lock (_sync) {
+        _binary_marker = _marker.Convert<Gray, byte>();
+        _marker_size = _binary_marker.Width;
+
+        // Warp points are specified in counter-clockwise order
+        // since cvApproxpoly seems to return poly-points in counter-clockwise order.
+        //
+        // 0   3
+        // +---+
+        // |   |
+        // +---+
+        // 1   2
+        _warp_dest = new PointF[] { 
+          new PointF(0, 0),
+          new PointF(0, _marker_size),
+          new PointF(_marker_size, _marker_size),
+          new PointF(_marker_size, 0)
+        };
+
+        // Storage to hold warped image
+        _warped = new Image<Gray, byte>(_marker_size, _marker_size);
+        // Storage to test orientations
+        _tmp = new Image<Gray, byte>(_marker_size, _marker_size);
+      }
+    }
+
+
+    public override bool FindPattern(Image<Gray, byte> img, out PointF[] image_points) {
+      image_points = null;
+
+      if (_marker == null) {
+        return false;
+      }
+
+      Contour<Point> contour_points;
+      // Find contour points in black/white image
+      using (Image<Gray, byte> binary = new Image<Gray, byte>(img.Size)) {
+        CvInvoke.cvThreshold(img, binary, _binary_threshold, 255, THRESH.CV_THRESH_BINARY | THRESH.CV_THRESH_OTSU);
+        binary._Not(); // Contour is searched on white points, marker envelope is black.
+        contour_points = binary.FindContours();
+      }
+      lock (_sync) { // Lock here in case the pattern is changed while using it.
+        bool marker_found = false;
+        double best_error = _max_error_normed;
+        while (contour_points != null) {
+          // Approximate contour points by poly-lines.
+          // For our marker-envelope should generate a poly-line consisting of four vertices.
+          Contour<Point> c = contour_points.ApproxPoly(contour_points.Perimeter * 0.05, _contour_storage);
+          if (c.Total == 4 && c.Perimeter > 200) {
+
+            // Warp content of poly-line as if looking at it from the top
+            PointF[] warp_source = new PointF[] { 
+              new PointF(c[0].X, c[0].Y),
+              new PointF(c[1].X, c[1].Y),
+              new PointF(c[2].X, c[2].Y),
+              new PointF(c[3].X, c[3].Y)
+            };
+
+            CvInvoke.cvGetPerspectiveTransform(warp_source, _warp_dest, _warp_matrix);
+            CvInvoke.cvWarpPerspective(
+              img, _warped, _warp_matrix,
+              (int)INTER.CV_INTER_CUBIC + (int)WARP.CV_WARP_FILL_OUTLIERS,
+              new MCvScalar(0)
+            );
+            CvInvoke.cvThreshold(_warped, _warped, _binary_threshold, 255, THRESH.CV_THRESH_BINARY | THRESH.CV_THRESH_OTSU);
+
+            // Perform a template matching with the stored pattern in order to
+            // determine if content of the envelope matches the stored pattern and
+            // determine the orientation of the pattern in the image.
+            // Orientation is encoded
+            // 0: 0°, 1: 90°, 2: 180°, 3: 270°
+            double error;
+            int orientation;
+            MatchPattern(out error, out orientation);
+
+            if (error <  best_error) {
+              best_error = error;
+              int id_0 = orientation;
+              int id_1 = (orientation + 1) % 4;
+              int id_2 = (orientation + 2) % 4;
+              int id_3 = (orientation + 3) % 4;
+
+              // ids above are still counterclockwise ordered, we need to permute them
+              // 0   3    0   1
+              // +---+    +---+
+              // |   | -> |   |
+              // +---+    +---+
+              // 1   2    2   3
+
+              image_points = new PointF[4];
+              image_points[0] = c[id_0];
+              image_points[1] = c[id_3];
+              image_points[2] = c[id_1];
+              image_points[3] = c[id_2];
+
+              marker_found = true;
+            }
+          }
+          contour_points = contour_points.HNext;
+        }
+        return marker_found;
+      }
+    }
+
+    private void MatchPattern(out double error, out int orientation) {
+      // 0 degrees
+      orientation = 0;
+      error = _warped.MatchTemplate(_binary_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0, 0].Intensity;
+
+      // 90 degrees
+      CvInvoke.cvTranspose(_warped, _tmp);
+      CvInvoke.cvFlip(_tmp, IntPtr.Zero, 1); // y-axis 
+      double err = _tmp.MatchTemplate(_binary_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0, 0].Intensity;
+      if (err < error) {
+        error = err;
+        orientation = 1;
+      }
+
+      // 180 degrees
+      CvInvoke.cvFlip(_warped, _tmp, -1);
+      err = _tmp.MatchTemplate(_binary_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0, 0].Intensity;
+      if (err < error) {
+        error = err;
+        orientation = 2;
+      }
+
+      // 270 degrees
+      CvInvoke.cvTranspose(_warped, _tmp);
+      CvInvoke.cvFlip(_tmp, IntPtr.Zero, 0); // x-axis 
+      err = _tmp.MatchTemplate(_binary_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0, 0].Intensity;
+      if (err < error) {
+        error = err;
+        orientation = 3;
+      }
+    }
+
+
+  }
+}
--- trunk/Playground/MarkerDetetection.cs
+++ trunk/Playground/MarkerDetetection.cs
@@ -12,85 +12,134 @@ using Emgu.CV.CvEnum;
 namespace Playground {
   [Parsley.Core.Addins.Addin]
   public class MarkerDetetection : Parsley.Core.IImageAlgorithm {
-    Emgu.CV.Image<Gray, byte> _marker;
+
+    enum Orientation {
+      Degrees0,
+      Degrees90,
+      Degrees180,
+      Degrees270
+    }
+
+    Emgu.CV.Image<Gray, byte> _marker, _roi, _tmp;
     int _marker_size;
+    PointF[] _dest;
 
     public MarkerDetetection() {
-      Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> m = new Image<Bgr, byte>("marker.bmp");
+      Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> m = new Image<Bgr, byte>("marker2.bmp");
       Emgu.CV.Image<Gray, byte> gray = m.Convert<Gray, byte>();
-      gray = gray.Resize(30, 30, INTER.CV_INTER_CUBIC);
+      gray = gray.Resize(30, 30, INTER.CV_INTER_LINEAR);
       gray._ThresholdBinary(new Gray(50), new Gray(255.0));
-      gray._Not();
       _marker = gray;
       _marker_size = _marker.Width;
+
+      _dest = new PointF[] { 
+            new PointF(0, 0),
+            new PointF(0, _marker_size),
+            new PointF(_marker_size, _marker_size),
+            new PointF(_marker_size, 0)
+            };
+
+      _roi = new Image<Gray,byte>(_marker_size, _marker_size);
+      _tmp = new Image<Gray, byte>(_marker_size, _marker_size);
     }
 
     #region IImageAlgorithm Members
 
     public void ProcessImage(Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> image) {
       Emgu.CV.Image<Gray, byte> gray = image.Convert<Gray, byte>();
-      gray.SmoothGaussian(3);
-      gray._ThresholdBinary(new Gray(50), new Gray(255.0));
-      gray._Not();
-      Emgu.CV.Contour<System.Drawing.Point> contour_points = gray.FindContours();
 
+      
+
+      Emgu.CV.Image<Gray, byte> binary  = new Image<Gray,byte>(image.Size);
+      CvInvoke.cvThreshold(gray, binary, 40, 255, THRESH.CV_THRESH_BINARY | THRESH.CV_THRESH_OTSU);
+      binary._Not();
+      Emgu.CV.Contour<System.Drawing.Point> contour_points = binary.FindContours();
+      
       MemStorage storage = new MemStorage();
       Matrix<double> warp = new Matrix<double>(3, 3);
-      PointF[] dest = new PointF[] { 
-            new PointF(0, 0),
-            new PointF(0, _marker_size),
-            new PointF(_marker_size, _marker_size),
-            new PointF(_marker_size, 0)
-            };
-      Emgu.CV.Image<Gray, byte> roi = new Image<Gray,byte>(_marker_size, _marker_size);
 
       while (contour_points != null) {
         Contour<Point> c = contour_points.ApproxPoly(contour_points.Perimeter * 0.05, storage);
         double p = c.Perimeter;
-        if (c.Total == 4 && p > 500) {
+        if (c.Total == 4 && p > 300) {
           
-
           PointF[] src = new PointF[] { 
             new PointF(c[0].X, c[0].Y),
             new PointF(c[1].X, c[1].Y),
             new PointF(c[2].X, c[2].Y),
             new PointF(c[3].X, c[3].Y)};
 
-          CvInvoke.cvGetPerspectiveTransform(src, dest, warp);
+          CvInvoke.cvGetPerspectiveTransform(src, _dest, warp);
           int flags = (int)INTER.CV_INTER_LINEAR + (int)WARP.CV_WARP_FILL_OUTLIERS;
-          CvInvoke.cvWarpPerspective(gray, roi, warp, flags, new MCvScalar(0));
+          CvInvoke.cvWarpPerspective(gray, _roi, warp, flags, new MCvScalar(0));
 
-          //Emgu.CV.Image<Gray, byte> roi = gray.WarpPerspective(warp, _marker_size, _marker_size, INTER.CV_INTER_LINEAR, WARP.CV_WARP_FILL_OUTLIERS, new Gray(0));
-          //roi._ThresholdBinary(new Gray(50), new Gray(255.0));
-          /*for (int i = 0; i < _marker_size; ++i) {
-            for (int j = 0; j < _marker_size; ++j) {
-              image[i + _marker_size, j] = new Bgr(roi[i, j].Intensity, roi[i, j].Intensity, roi[i, j].Intensity);
-              image[i, j] = new Bgr(_marker[i, j].Intensity, _marker[i, j].Intensity, _marker[i, j].Intensity);
-            }
-          }*/
-         
-          
-          Emgu.CV.Image<Gray, float> positions = roi.MatchTemplate(_marker, TM_TYPE.CV_TM_SQDIFF_NORMED);
-          double error = positions[0,0].Intensity;
-          Console.WriteLine(error);
-          if (error < 0.3) {
+
+          double min_error;
+          Orientation orient;
+
+          FindBestOrientation(out min_error, out orient);
+          if (min_error < 0.4) {
             image.DrawPolyline(c.ToArray(), true, new Bgr(Color.Green), 2);
+            System.Console.WriteLine(min_error + " " + orient);
+
+            switch (orient) {
+              case Orientation.Degrees0:
+                image.Draw(new LineSegment2D(c[0], c[3]), new Bgr(System.Drawing.Color.Red), 2);
+                break;
+              case Orientation.Degrees90:
+                image.Draw(new LineSegment2D(c[1], c[0]), new Bgr(System.Drawing.Color.Red), 2);
+                break;
+              case Orientation.Degrees180:
+                image.Draw(new LineSegment2D(c[2], c[1]), new Bgr(System.Drawing.Color.Red), 2);
+                break;
+              case Orientation.Degrees270:
+                image.Draw(new LineSegment2D(c[3], c[2]), new Bgr(System.Drawing.Color.Red), 2);
+                break;
+            }
+            
+
+
           }
+
+          // 0 degrees
         }
         contour_points = contour_points.HNext;
       }
+    }
 
-      /*
-       * rotate
-       *     // rotate image
-        CvPoint2D32f center = cvPoint2D32f(frame->width / 2, frame->height / 2);
-        angle = 90;
-        scale = 1;
-        cv2DRotationMatrix(center, angle, scale, rot_mat);
-        // transform
-        cvWarpAffine(frame, frame_rot, rot_mat,CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS,     cvScalarAll(0));
-       * */
+    private void FindBestOrientation(out double min_error, out Orientation orient) {
 
+      
+
+      // 0 degrees
+      orient = Orientation.Degrees0;
+      min_error = _roi.MatchTemplate(_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0,0].Intensity;
+
+      // 90 degrees
+      CvInvoke.cvTranspose(_roi, _tmp);
+      CvInvoke.cvFlip(_tmp, IntPtr.Zero, 1); // y-axis 
+      double error = _tmp.MatchTemplate(_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0, 0].Intensity;
+      if (error < min_error) {
+        min_error = error;
+        orient = Orientation.Degrees90;
+      }
+
+      //180 degrees
+      CvInvoke.cvFlip(_roi, _tmp, -1);
+      error = _tmp.MatchTemplate(_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0, 0].Intensity;
+      if (error < min_error) {
+        min_error = error;
+        orient = Orientation.Degrees180;
+      }
+
+      //270 degrees
+      CvInvoke.cvTranspose(_roi, _tmp);
+      CvInvoke.cvFlip(_tmp, IntPtr.Zero, 0); // x-axis 
+      error = _tmp.MatchTemplate(_marker, TM_TYPE.CV_TM_SQDIFF_NORMED)[0, 0].Intensity;
+      if (error < min_error) {
+        min_error = error;
+        orient = Orientation.Degrees270;
+      }
 
     }
 
