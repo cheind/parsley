74ecd4f938e103ece0c1f7d80fd4720c5d31ba4c
matthias.plasch <matthias.plasch@0309ed38-fbab-11de-acd2-a780054a256f>
2010-08-04 14:23:05 +0000

    added Interface IPositioner. The classes RotaryPositioner and MarkerPositioner need to implement the given interface functions "TransformPoints" (perform point transformation from the rotated/translated system to the original Positioner coordinate system), "PositionerPose" (sets and returns the extrinsic calibration parameters), "Angle" (sets and returns the current rotational angle).

---
+++ trunk/Parsley.Core/IPositioner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Parsley.Core
+{
+  public interface IPositioner
+  {
+
+    void TransformPoints(List<Vector> points);
+
+    Emgu.CV.ExtrinsicCameraParameters PositionerPose
+    {
+      get;
+      set;
+    }
+
+    double Angle
+    {
+      get;
+      set;
+    }
+  }
+}
