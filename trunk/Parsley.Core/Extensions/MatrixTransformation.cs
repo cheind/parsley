5c96538a14d2c584805dcf84538ae7d609400cad
matthias.plasch <matthias.plasch@0309ed38-fbab-11de-acd2-a780054a256f>
2010-09-06 11:13:02 +0000

    added static class MatrixTransformation which provides the static functions TransformVectorToVector and TransformVectorToEmgu. ** TransformVectorToVector: Transforms the given IEnumerable object (e.g. List<Vector>) using the TransformationMatrix (second parameter). An IEnumerable<Vector> object is returned. ** TransformVectorToEmgu: Transforms the given IEnumerable object (e.g. List<Vector>) using the TransformationMatrix (second parameter). An IEnumerable<MCVPoint3D32f> object is returned.

---
+++ trunk/Parsley.Core/Extensions/MatrixTransformation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parsley.Core.Extensions
+{
+  public static class MatrixTransformation
+  {
+    public static IEnumerable<MathNet.Numerics.LinearAlgebra.Vector> TransformVectorToVector(MathNet.Numerics.LinearAlgebra.Matrix TMatrix, double w,
+                                                                                      IEnumerable<MathNet.Numerics.LinearAlgebra.Vector> PointVector)
+    {
+      foreach(MathNet.Numerics.LinearAlgebra.Vector v in PointVector)
+      {
+        MathNet.Numerics.LinearAlgebra.Matrix colMatrix = new MathNet.Numerics.LinearAlgebra.Matrix(4, 1);
+        colMatrix[3, 0] = w;
+        colMatrix.SetMatrix(0, 2, 0, 0, v.ToColumnMatrix());
+
+        yield return (TMatrix.Multiply(colMatrix)).GetColumnVector(0).ToNonHomogeneous();
+      }
+    }
+
+    public static IEnumerable<Emgu.CV.Structure.MCvPoint3D32f> TransformVectorToEmgu(MathNet.Numerics.LinearAlgebra.Matrix TMatrix, double w,
+                                                                                IEnumerable<MathNet.Numerics.LinearAlgebra.Vector> PointVector)
+    {
+      foreach (MathNet.Numerics.LinearAlgebra.Vector v in PointVector)
+      {
+        MathNet.Numerics.LinearAlgebra.Matrix colMatrix = new MathNet.Numerics.LinearAlgebra.Matrix(4, 1);
+        colMatrix[3, 0] = w;
+        colMatrix.SetMatrix(0, 2, 0, 0, v.ToColumnMatrix());
+
+        yield return (TMatrix.Multiply(colMatrix)).GetColumnVector(0).ToNonHomogeneous().ToEmguF();
+      }
+    }
+  }
+}
